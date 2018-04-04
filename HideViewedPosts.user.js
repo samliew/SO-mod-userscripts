4c5a24f6e878008f0a517b4ad39eae5c6ee6d251
Samuel Liew <samliew@gmail.com>
2018-04-04 21:49:48 +1000

    Create HideViewedPosts.user.js
    Avoid posts already seen and possibly handled by another moderator

---
+++ HideViewedPosts.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Hide viewed posts
+// @description  Avoid posts already seen and possibly handled by another moderator
+// @match        https://stackoverflow.com/admin/dashboard*
+// @match        https://meta.stackoverflow.com/admin/dashboard*
+// @match        https://*.stackexchange.com/admin/dashboard?flag*=comment*
+// @author       @samliew
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Avoid posts already seen and possibly handled by another moderator
+    $('.mod-audit').filter((i,e) => e.innerText.indexOf('question viewed by:') >= 0).closest('.flagged-post-row').remove();
+
+})();
