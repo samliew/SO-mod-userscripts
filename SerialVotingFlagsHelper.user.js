d3c90f4401553f94379dac5fdc23ea61fc83d99b
Samuel Liew <samliew@gmail.com>
2018-04-09 12:15:39 +1000

    Add styles to SerialVotingFlagsHelper

--- SerialVotingFlagsHelper.user.js
+++ SerialVotingFlagsHelper.user.js
@@ -15,12 +15,26 @@
 
             // Add Votes and IP-xref links after the user link
             var uid = this.href.match(/\d+/);
-            $('<span>&nbsp;</span><br>(<a href="https://stackoverflow.com/users/account-info/'+uid+'" target="_blank">mod</a>|<a href="https://stackoverflow.com/admin/show-user-votes/'+uid+'" target="_blank">votes</a>|<a href="https://stackoverflow.com/admin/xref-user-ips/'+uid+'" target="_blank">xref</a>)<span>&nbsp;</span>')
+            $('<div class="mod-userlinks">[ <a href="https://stackoverflow.com/users/account-info/'+uid+'" target="_blank">mod</a> | <a href="https://stackoverflow.com/admin/show-user-votes/'+uid+'" target="_blank">votes</a> | <a href="https://stackoverflow.com/admin/xref-user-ips/'+uid+'" target="_blank">xref</a> ]</div>')
                 .insertAfter(this);
         });
     }
 
+    function appendStyles() {
+
+        var styles = `
+<style>
+.mod-userlinks {
+    font-size: 0.85em;
+    text-transform: uppercase;
+}
+</style>
+`;
+        $('body').append(styles);
+    }
+
     // On page load
     doPageload();
+    appendStyles();
 
 })();
