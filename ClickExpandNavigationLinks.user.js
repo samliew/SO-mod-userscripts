e269ca48afe49a0d0dc27a9e66ac444def3f5d2a
Samuel Liew <samliew@gmail.com>
2018-04-10 14:26:51 +1000

    Create ClickExpandNavigationLinks.user.js

---
+++ ClickExpandNavigationLinks.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Click expand navigation links
+// @description  On pagination dots click, adds all in-between page links
+// @match        https://stackoverflow.com/*
+// @match        https://meta.stackoverflow.com/*
+// @match        https://*.stackexchange.com/*
+// @author       @samliew
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function doPageload() {
+
+        $('.page-numbers.dots').click(function() {
+
+            var prevNum = +($(this).prev().text());
+            var nextNum = +($(this).next().text());
+            for(let i = prevNum + 1; i < nextNum; i++) {
+                $(`<a href="?page=${i}" title="go to page ${i}"> <span class="page-numbers">${i}</span> </a>`).insertBefore(this);
+            }
+            $(this).remove();
+        });
+    }
+
+    function appendStyles() {
+
+        var styles = `
+<style>
+.page-numbers.dots:hover {
+    background-color: #f69c55;
+    color: white;
+    cursor: pointer;
+}
+</style>
+`;
+        $('body').append(styles);
+    }
+
+    // On page load
+    doPageload();
+    appendStyles();
+
+})();
