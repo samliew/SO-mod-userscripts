daf1822c33c8e35f7c3390a920d007e048d86918
Samuel Liew <samliew@gmail.com>
2018-04-14 20:43:06 +1000

    Update HoverExpandNavigationLinks.user.js

--- HoverExpandNavigationLinks.user.js
+++ HoverExpandNavigationLinks.user.js
@@ -1,11 +1,23 @@
 // ==UserScript==
 // @name         Hover Expand Navigation Links
 // @description  On pagination dots "..." mouseover, adds all in-between page links
-// @match        https://stackoverflow.com/*
-// @match        https://meta.stackoverflow.com/*
-// @match        https://*.stackexchange.com/*
+// @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.0
+// @version      1.2
+//
+// @include      https://stackoverflow.com/*
+// @include      https://serverfault.com/*
+// @include      https://superuser.com/*
+// @include      https://askubuntu.com/*
+// @include      https://mathoverflow.net/*
+//
+// @include      https://meta.stackoverflow.com/*
+// @include      https://meta.serverfault.com/*
+// @include      https://meta.superuser.com/*
+// @include      https://meta.askubuntu.com/*
+// @include      https://meta.mathoverflow.net/*
+//
+// @include      /^https?:\/\/.*\.stackexchange\.com\/.*/
 // ==/UserScript==
 
 (function() {
