a6a3004cb1497cf10dfbe9ef45c8f65f07126691
Samuel Liew <samliew@gmail.com>
2018-04-18 15:39:10 +1000

    Update ModPopupDialogImprovements.user.js

--- ModPopupDialogImprovements.user.js
+++ ModPopupDialogImprovements.user.js
@@ -3,7 +3,7 @@
 // @description  Some simple improvements for posts' Mod popup dialog
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.1
+// @version      1.2
 //
 // @match        https://*stackoverflow.com/*
 // @match        https://meta.stackoverflow.com/*
@@ -15,11 +15,15 @@
 
     function listenToPageUpdates() {
         $(document).ajaxComplete(function() {
+
             var $popupForm = $('.popup._hidden-descriptions form');
             if($popupForm) {
 
                 // Move comments to chat is selected by default
                 $('#tab-actions input[value="move-comments-to-chat"]').prop('checked', true).triggerHandler('click');
+
+                // On postlowquality queue, default to convert-to-comment instead
+                $('#tab-actions input[value="convert-to-comment"]').prop('checked', true).triggerHandler('click');
 
                 // Delete moved comments is checked by default
                 $('#delete-moved-comments').prop('checked', true);
@@ -28,6 +32,11 @@
                 if(location.href.indexOf('/admin/dashboard') >= 0) {
                     $popupForm.attr('target', '_blank');
                 }
+
+                // Submitting popup form hides the post as well
+                $popupForm.on('click', 'input:submit', function() {
+                    $(this).parents('tr.flagged-post-row').hide();
+                });
             }
         });
     }
