c5039bd61f768cf0968c203cb889babe4a4394b1
Samuel Liew <samliew@gmail.com>
2018-04-19 10:38:05 +1000

    Create TooManyCommentsFlagQueueHelper.user.js

---
+++ TooManyCommentsFlagQueueHelper.user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         Too Many Comments Flag Queue Helper
+// @description  Displays only the important info (comments) and actions (Mod popup)
+// @homepage     https://github.com/samliew/SO-mod-userscripts
+// @author       @samliew
+// @version      1.0
+//
+// @match        */admin/dashboard?flagtype=posttoomanycommentsauto
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function doPageLoad() {
+
+        // Expand unhandled posts
+        var unhandledPosts = $('.flagged-post-row').filter((i,el) => $('.mod-post-actions', el).text().indexOf('ModMovedCommentsToChat') === -1);
+        setTimeout((unhandledPosts) => $('.expand-body', unhandledPosts).click(), 300, unhandledPosts);
+
+        // Display "No further action" for handled posts
+        $('.flagged-post-row').not(unhandledPosts).find('.post-options').show().find('input').not('.dismiss-all').hide();
+    }
+
+    function listenToPageUpdates() {
+        $(document).ajaxComplete(function() {
+            // Always expand comments if post is expanded
+            $('.js-show-link.comments-link').trigger('click');
+        });
+    }
+
+    function appendStyles() {
+
+        var styles = `
+<style>
+.post-text,
+.post-taglist,
+.post-options.keep,
+.dismiss-options,
+.mod-message {
+    display: none;
+}
+.tagged-ignored {
+    opacity: 1;
+}
+</style>
+`;
+        $('body').append(styles);
+    }
+
+    // On page load
+    appendStyles();
+    doPageLoad();
+    listenToPageUpdates();
+
+})();
