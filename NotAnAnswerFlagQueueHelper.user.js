cdea56c8a564f9e5f2b90f705c59648bb0d79491
Samuel Liew <samliew@gmail.com>
2018-04-30 15:12:34 +1000

    Version 2.0: Inserts several sort options for the NAA queue
    Previously sorted by poster rep by default; now it's one of the available sort options.

--- NotAnAnswerFlagQueueHelper.user.js
+++ NotAnAnswerFlagQueueHelper.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Not An Answer Flag Queue Helper
-// @description  Sorts NAA posts by poster rep
+// @description  Inserts several sort options for the NAA queue
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.0
+// @version      2.0
 //
 // @match        */admin/dashboard?flagtype=answernotananswer*
 // ==/UserScript==
@@ -11,25 +11,159 @@
 (function() {
     'use strict';
 
-    function doPageLoad() {
+    var $postsContainer, $posts;
 
-        var $tbody = $('.flagged-post-row').first().parent();
 
-        // Sort flags
-        var $posts = $('.flagged-post-row')
-            .sort(function(a, b) {
-                var aRep = Number($(a).find('.reputation-score').text().replace(/[^\d.]/g, '')),
-                    bRep = Number($(b).find('.reputation-score').text().replace(/[^\d.]/g, ''));
-                if(aRep % 1 > 0) aRep *= 1000;
-                if(bRep % 1 > 0) bRep *= 1000;
+    function sortPosts(filter) {
+        console.log("Sort by: " + filter);
 
-                if(aRep == bRep) return 0;
-                return (aRep > bRep) ? 1 : -1;
-            });
-        $posts.detach().appendTo($tbody);
+        // Get sort function based on selected filter
+        let sortFunction;
+
+        switch(filter) {
+
+            case 'poster-rep':
+                sortFunction = function(a, b) {
+                    let aRep = Number($(a).find('.reputation-score').text().replace(/[^\d.]/g, '')),
+                        bRep = Number($(b).find('.reputation-score').text().replace(/[^\d.]/g, ''));
+                    if(aRep % 1 > 0) aRep *= 1000;
+                    if(bRep % 1 > 0) bRep *= 1000;
+
+                    if(aRep == bRep) return 0;
+                    return (aRep > bRep) ? 1 : -1;
+                };
+                break;
+
+            case 'self-answer':
+                sortFunction = function(a, b) {
+                    let aOwner = $(a).find('.mod-audit p:last').text(),
+                        bOwner = $(b).find('.mod-audit p:last').text();
+
+                    return aOwner === '(answering own question)' ? -1 : 0;
+                };
+                break;
+
+            case 'date-posted':
+                sortFunction = function(a, b) {
+                    let aDate = new Date($(a).find('.user-action-time .relativetime').attr('title')),
+                        bDate = new Date($(b).find('.user-action-time .relativetime').attr('title'));
+
+                    if(aDate == bDate) return 0;
+                    return (aDate > bDate) ? 1 : -1;
+                };
+                break;
+
+            case 'delete-votes':
+                sortFunction = function(a, b) {
+                    let aDelv = Number($(a).find('.delete-post').val().replace(/[^\d]/g, '')),
+                        bDelv = Number($(b).find('.delete-post').val().replace(/[^\d]/g, ''));
+
+                    if(aDelv == bDelv) return 0;
+                    return (aDelv < bDelv) ? 1 : -1;
+                };
+                break;
+
+            case 'flag-count':
+                sortFunction = function(a, b) {
+                    let aFlags = Number($(a).find('.dismiss-options .bounty-indicator-tab').text()),
+                        bFlags = Number($(b).find('.dismiss-options .bounty-indicator-tab').text());
+
+                    if(aFlags == bFlags) return 0;
+                    return (aFlags < bFlags) ? 1 : -1;
+                };
+                break;
+
+            case 'post-length':
+                sortFunction = function(a, b) {
+                    let aLen = $(a).find('.post-summary-body').text().length,
+                        bLen = $(b).find('.post-summary-body').text().length;
+
+                    if(aLen == bLen) return 0;
+                    return (aLen > bLen) ? 1 : -1;
+                };
+                break;
+
+            default:
+                location.reload(true);
+                return;
+        }
+
+        // Sort posts in-memory then reattach to container
+        $posts.sort(sortFunction).detach().appendTo($postsContainer);
     }
 
+
+    function doPageLoad() {
+
+        $postsContainer = $('.flagged-post-row').first().parent();
+        $posts = $('.flagged-post-row');
+
+        // Insert sort options
+        const $filterOpts = $(`<div id="flag-queue-tabs" class="tabs">
+                <a data-filter="default" class="youarehere">Default</a>
+                <a data-filter="self-answer">Self Answer</a>
+                <a data-filter="poster-rep">Poster Rep</a>
+                <a data-filter="date-posted">Date Posted</a>
+                <a data-filter="delete-votes">Delete Votes</a>
+                <a data-filter="flag-count">Flag Count</a>
+                <a data-filter="post-length">Post Length</a>
+            </div>`);
+        $('#chk-apply-filters').parent().append($filterOpts);
+
+        // Sort options event
+        $('#flag-queue-tabs').on('click', 'a[data-filter]', function() {
+            sortPosts(this.dataset.filter);
+
+            // Update active tab highlight class
+            $(this).addClass('youarehere').siblings().removeClass('youarehere');
+
+            return false;
+        });
+    }
+
+
+    function appendStyles() {
+
+        const styles = `
+<style>
+.tabs:after,
+#tabs:after {
+    content: '';
+    clear: both;
+    display: block;
+}
+.tabs .youarehere,
+#tabs .youarehere {
+    position: relative;
+    z-index: 1;
+}
+#flag-queue-tabs {
+    float: none;
+    margin: 20px 0 30px;
+}
+#flag-queue-tabs:after {
+    position: relative;
+    top: -1px;
+    border-bottom: 1px solid #e4e6e8;
+}
+#flag-queue-tabs a {
+    float: left;
+    margin-right: 8px;
+    padding: 12px 8px 14px;
+    color: #848d95;
+    line-height: 1;
+    text-decoration: none;
+    border-bottom: 2px solid transparent;
+    transition: all .15s ease-in-out;
+}
+</style>
+`;
+        $('body').append(styles);
+    }
+
+
     // On page load
+    appendStyles();
     doPageLoad();
 
 })();
