2e87d1db1dadea521732401d7c3e579e205efb0b
Samuel Liew <samliew@gmail.com>
2018-05-19 13:02:07 +1000

    Fix history overlapping buttons

--- CommentFlagsHelper.user.js
+++ CommentFlagsHelper.user.js
@@ -3,7 +3,7 @@
 // @description  Always expand comments (with deleted) and highlight expanded flagged comments, Highlight common chatty and rude keywords
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.11
+// @version      1.12
 //
 // @include      https://stackoverflow.com/admin/dashboard?flag*=comment*
 // @include      https://serverfault.com/admin/dashboard?flag*=comment*
@@ -252,7 +252,7 @@
 }
 #mod-history {
     position: absolute;
-    top: 0;
+    top: 40px;
     max-height: 150px;
     overflow-y: auto;
     background: white;
