ee459b589453848392f7e3f5c665cbd363d6ddcc
Samuel Liew <samliew@gmail.com>
2018-05-31 09:45:26 +1000

    Works for mod flag queue as well

--- ChatTranscriptsByDefault.user.js
+++ ChatTranscriptsByDefault.user.js
@@ -3,31 +3,54 @@
 // @description  Rewrites chat room links in comments to chat transcript, to avoid joining the room
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.0.1
+// @version      1.1
 //
-// @match        https://*stackoverflow.com/questions/*
-// @match        https://*serverfault.com/questions/*
-// @match        https://*superuser.com/questions/*
-// @match        https://*askubuntu.com/questions/*
-// @match        https://*mathoverflow.net/questions/*
-// @match        https://*meta.stackexchange.com/questions/*
+// @include      https://*stackoverflow.com/questions/*
+// @include      https://*serverfault.com/questions/*
+// @include      https://*superuser.com/questions/*
+// @include      https://*askubuntu.com/questions/*
+// @include      https://*mathoverflow.net/questions/*
+// @include      https://*.stackexchange.com/questions/*
+//
+// @include      https://*stackoverflow.com/admin/dashboard*
+// @include      https://*serverfault.com/admin/dashboard*
+// @include      https://*superuser.com/admin/dashboard*
+// @include      https://*askubuntu.com/admin/dashboard*
+// @include      https://*mathoverflow.net/admin/dashboard*
+// @include      https://*.stackexchange.com/admin/dashboard*
 // ==/UserScript==
 
 (function() {
     'use strict';
 
 
-    function doPageLoad() {
-        $('.comment-copy a').filter((i, el) => el.href.indexOf('chat.') >= 0 && el.href.indexOf('/rooms/') >= 0)
+    function convertChatLinksToTranscript() {
+
+        // For each link in comments, where url matches chat + rooms
+        $('.comment-copy a')
+            .filter((i, el) => el.href.indexOf('chat.') >= 0 && el.href.indexOf('/rooms/') >= 0)
             .attr('href', (i, v) => v.replace('/rooms/', '/transcript/'))
             .attr({
                 target: '_blank',
-                title: 'Opens in a new window'
-            });
+                title: 'Open chat transcript in a new window'
+            })
+            .html((i, v) => v + ' <em>(view transcript)</em>');
+    }
+
+
+    function listenToPageUpdates() {
+
+        // On any page update
+        $(document).ajaxComplete(function(event, xhr, settings) {
+
+            // If comments have updated
+            if(settings.url.indexOf('/comments') >= 0) convertChatLinksToTranscript();
+        });
     }
 
 
     // On page load
-    doPageLoad();
+    convertChatLinksToTranscript();
+    listenToPageUpdates();
 
 })();
