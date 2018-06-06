52cc17f067084b5a4a23408a13c74f9b41e8dddd
Samuel Liew <samliew@gmail.com>
2018-06-06 15:45:23 +1000

    Update HoverExpandNavigationLinks.user.js

--- HoverExpandNavigationLinks.user.js
+++ HoverExpandNavigationLinks.user.js
@@ -1,35 +1,29 @@
 // ==UserScript==
 // @name         Hover Expand Navigation Links
-// @description  On pagination dots "..." mouseover, adds in-between page links (max 30)
+// @description  On pagination dots "..." mouseover, adds more page links (max 30 per hover)
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.3.1
+// @version      1.4
 //
-// @include      https://stackoverflow.com/*
-// @include      https://serverfault.com/*
-// @include      https://superuser.com/*
-// @include      https://askubuntu.com/*
-// @include      https://mathoverflow.net/*
-//
-// @include      https://meta.stackoverflow.com/*
-// @include      https://meta.serverfault.com/*
-// @include      https://meta.superuser.com/*
-// @include      https://meta.askubuntu.com/*
-// @include      https://meta.mathoverflow.net/*
-//
-// @include      /^https?:\/\/.*\.stackexchange\.com\/.*/
+// @include      https://*stackoverflow.com/*
+// @include      https://*serverfault.com/*
+// @include      https://*superuser.com/*
+// @include      https://*askubuntu.com/*
+// @include      https://*mathoverflow.net/*
+// @include      https://*stackexchange.com/*
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+
     function doPageload() {
 
         $('.page-numbers.dots').on('click mouseover', null, function() {
 
-            var prevNum = +($(this).prev().text());
-            var nextNum = +($(this).next().text());
-            var removeWhenDone = true;
+            let prevNum = +(this.previousElementSibling.innerText);
+            let nextNum = +(this.nextElementSibling.innerText);
+            let removeWhenDone = true;
             if(nextNum - prevNum > 30) {
                 nextNum = prevNum + 30;
                 removeWhenDone = false;
@@ -43,22 +37,8 @@
         });
     }
 
-    function appendStyles() {
-
-        var styles = `
-<style>
-.page-numbers.dots:hover {
-    background-color: #f69c55;
-    color: white;
-    cursor: pointer;
-}
-</style>
-`;
-        $('body').append(styles);
-    }
 
     // On page load
     doPageload();
-    appendStyles();
 
 })();
