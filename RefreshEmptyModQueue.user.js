7f175542b0583ee432563bb427f811927ae95ad9
Samuel Liew <samliew@gmail.com>
2018-06-18 00:12:05 +0800

    Update RefreshEmptyModQueue.user.js

--- RefreshEmptyModQueue.user.js
+++ RefreshEmptyModQueue.user.js
@@ -3,9 +3,14 @@
 // @description  If current mod queue is empty, reload page occasionally
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.0
+// @version      1.1
 //
-// @include      */admin/dashboard*
+// @include      https://*stackoverflow.com/*
+// @include      https://*serverfault.com/*
+// @include      https://*superuser.com/*
+// @include      https://*askubuntu.com/*
+// @include      https://*mathoverflow.net/*
+// @include      https://*.stackexchange.com/*
 // ==/UserScript==
 
 (function() {
@@ -18,14 +23,36 @@
 
     function doPageload() {
 
-        // If completely no post flags, redirect to main
-        if($('.so-flag, .m-flag, .c-flag').length === 0) {
-            setTimeout(goToMain, timeoutSecs * 1000);
+        // If no mod flags, insert mod flags indicator in header anyway...
+        if($('.flag-count-item').length === 0) {
+            $('.js-mod-inbox-button').parent().after(`<li class="-item flag-count-item" data-remove-order="3">
+                   <a href="/admin/dashboard" class="-link _text-only" title="no flags!">
+                       <span class="indicator-badge _regular">0</span>
+                   </a>
+               </li>`);
         }
 
-        // Otherwise refresh if no flags in current queue
-        else if($('.flagged-posts.moderator').children().length === 0) {
-            setTimeout(reloadPage, timeoutSecs * 1000);
+        // If on mod flag pages
+        if($('body').hasClass('flag-page')) {
+
+            // If completely no post flags, redirect to main
+            if($('.so-flag, .m-flag, .c-flag').length === 0) {
+                setTimeout(goToMain, timeoutSecs * 1000);
+            }
+
+            // Refresh if no flags in current queue
+            else if($('.flagged-posts.moderator').children().length === 0) {
+                setTimeout(reloadPage, timeoutSecs * 1000);
+            }
+
+            // When flags are handled,
+            $(document).ajaxStop(function(event, xhr, settings) {
+
+                // Refresh if no flags in current queue
+                if($('.flagged-post-row').length === 0) {
+                    setTimeout(reloadPage, timeoutSecs * 1000);
+                }
+            });
         }
     }
 
