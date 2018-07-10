7e092623ac5443671da1a989785cb82a19f02d0d
Samuel Liew <samliew@gmail.com>
2018-07-11 09:42:41 +1000

    Also collapse user signature (use tiny-signature)

--- NoOneboxesInChat.user.js
+++ NoOneboxesInChat.user.js
@@ -3,7 +3,7 @@
 // @description  Collapses oneboxes from chat rooms/transcripts/bookmarks, click to display onebox
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.0
+// @version      1.1
 //
 // @include      https://chat.stackoverflow.com/rooms/*
 // @include      https://chat.stackexchange.com/rooms/*
@@ -30,27 +30,33 @@
     unsafeWindow.hideOneboxes = function(mid = null) {
 
         // Display original link and hide oneboxes who hasn't been hidden before
-        let obs = $('.onebox').not('.js-onebox-hidden').addClass('js-onebox-hidden').hide().each(function() {
+        $('.onebox').not('.js-onebox-hidden').addClass('js-onebox-hidden').hide().each(function() {
+
+            // Onebox permalink is usually the first URL in the onebox
             let url = $(this).find('a').first().attr('href');
+
+            // If onebox type is a tweet, permalink is the last link in onebox
             if($(this).hasClass('ob-tweet')) url = $(this).find('a').last().attr('href');
 
+            // Click placeholder to show onebox
             $(`<span class="has-onebox" title="click to load onebox">${url}</span>`)
                 .click(function() {
                     $(this).addClass('js-show-onebox');
                 }).insertBefore(this);
+
+            // Also collapse user signature (use tiny-signature)
+            $(this).parents('.monologue').find('.tiny-signature').show().siblings().hide();
         });
 
-        let obs2 = [];
+        // Re-hide oneboxes if mid is set
         if(mid === -1) {
             // Re-hide all expanded oneboxes
-            obs2 = $('.js-show-onebox').removeClass('js-show-onebox');
+            $('.js-show-onebox').removeClass('js-show-onebox');
         }
-        else {
+        else if(mid) {
             // Re-hide specific message's onebox
-            obs2 = $(`#message-${mid}`).find('.js-show-onebox').removeClass('js-show-onebox');
+            $(`#message-${mid}`).find('.js-show-onebox').removeClass('js-show-onebox');
         }
-
-        //console.log((obs.length + obs2.length) + ' oneboxes hidden');
     };
 
 
@@ -60,7 +66,7 @@
         hideOneboxes();
 
         // Occasionally, look for new oneboxes and hide them
-        setInterval(hideOneboxes, 2000);
+        setInterval(hideOneboxes, 1000);
     }
 
 
