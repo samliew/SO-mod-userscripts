4969c1fb87942bb46c119e41701ee23e5080554e
Samuel Liew <samliew@gmail.com>
2018-07-19 08:56:31 +1000

    Create ReduceClutter.user.js

---
+++ ReduceClutter.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Reduce Clutter
+// @description  Reveals comment action icons on comment hover
+// @homepage     https://github.com/samliew/SO-mod-userscripts
+// @author       @samliew
+// @version      1.0
+//
+// @include      https://*stackoverflow.com/*
+// @include      https://*serverfault.com/*
+// @include      https://*superuser.com/*
+// @include      https://*askubuntu.com/*
+// @include      https://*mathoverflow.net/*
+// @include      https://*.stackexchange.com/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+    function appendStyles() {
+
+        const styles = `
+<style>
+ul.comments-list .comment-voting,
+ul.comments-list .comment-flagging {
+    visibility: hidden;
+}
+ul.comments-list .comment:hover .comment-voting,
+ul.comments-list .comment:hover .comment-flagging {
+    visibility: visible;
+}
+</style>
+`;
+        $('body').append(styles);
+    }
+
+
+    // On page load
+    appendStyles();
+
+})();
