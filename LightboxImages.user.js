73ad6be3b076d09d04abee985570fffee972eea0
Samuel Liew <samliew@gmail.com>
2018-08-15 11:06:34 +1000

    CSS

--- LightboxImages.user.js
+++ LightboxImages.user.js
@@ -3,7 +3,7 @@
 // @description  Opens image links in a lightbox instead of new window/tab in main & chat
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.1.1
+// @version      1.2
 //
 // @include      https://*stackoverflow.com/*
 // @include      https://*serverfault.com/*
@@ -16,6 +16,9 @@
 
 (function() {
     'use strict';
+
+
+    const lbSelector = '.ob-image a, a[href$=jpg], a[href$=png], a[href$=gif]';
 
 
     $.getCachedScript = function(url, callback) {
@@ -33,13 +36,39 @@
         $(`<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css">`).appendTo(document.body);
         $.getCachedScript('https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js', function() {
             $().fancybox({
-                selector : '.ob-image a, a[href$=jpg], a[href$=png], a[href$=gif]'
+                selector : lbSelector
             });
         });
+
+        // For text links to images, also visually display an indicator
+        $(lbSelector).addClass('image-lightbox');
+    }
+
+
+    function appendStyles() {
+
+        const styles = `
+<style>
+.image-lightbox {
+    cursor: zoom-in;
+}
+.fancybox-container button {
+    box-shadow: none;
+}
+.fancybox-container button:hover {
+    background: rgba(30,30,30,.6);
+}
+.fancybox-container button[disabled] {
+    visibility: hidden;
+}
+</style>
+`;
+        $('body').append(styles);
     }
 
 
     // On page load
+    appendStyles();
     doPageload();
 
 })();
