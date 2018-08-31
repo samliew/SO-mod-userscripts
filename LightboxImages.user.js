68cbd2390b6ecc6e8aae8750db21aa21a3edd873
Samuel Liew <samliew@gmail.com>
2018-08-31 14:44:39 +1000

    Lightbox images that are displayed smaller than it's original size

--- LightboxImages.user.js
+++ LightboxImages.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Lightbox Images
-// @description  Opens image links in a lightbox instead of new window/tab in main & chat
+// @description  Opens image links in a lightbox instead of new window/tab in main & chat. Lightbox images that are displayed smaller than it's original size.
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.2.1
+// @version      1.3
 //
 // @include      https://*stackoverflow.com/*
 // @include      https://*serverfault.com/*
@@ -18,7 +18,7 @@
     'use strict';
 
 
-    const lbSelector = '.ob-image a, a[href$=.jpg], a[href$=.png], a[href$=.gif]';
+    const lbSelector = '.ob-image a, a[href$=".jpg"], a[href$=".png"], a[href$=".gif"]';
 
 
     $.getCachedScript = function(url, callback) {
@@ -35,7 +35,27 @@
         // Imgur album link to direct image
         $('a[href^="https://imgur.com/"]').attr('href', (i,v) => v.match(/\.(jpg|png|gif)$/) != null ? v : v + '.jpg');
 
-        /* Load fancybox 3 - https://fancyapps.com/fancybox/3/docs/#options */
+        // If unlinked images' width is greater than displayed width, also lightbox the image
+        $('img').filter(function() {
+            return typeof this.parentNode.href === 'undefined';
+        }).wrap(function() {
+            return `<a class="unlinked-image" data-src="${this.src}"></a>`;
+        });
+        $(window).on('load resize', function() {
+            $('.unlinked-image').each(function() {
+                const img = this.children[0];
+                if(img.width < img.naturalWidth) {
+                    this.href = this.dataset.src;
+                    this.classList.add('image-lightbox');
+                }
+                else {
+                    this.removeAttribute('href');
+                    this.classList.remove('image-lightbox');
+                }
+            });
+        });
+
+        // Load fancybox 3 - https://fancyapps.com/fancybox/3/docs/#options
         $(`<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css">`).appendTo(document.body);
         $.getCachedScript('https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js', function() {
             $().fancybox({
@@ -52,6 +72,9 @@
 
         const styles = `
 <style>
+.unlinked-image {
+    cursor: default;
+}
 .image-lightbox {
     cursor: zoom-in;
 }
