84174e04737d3a1373ac8c19ddaa34d8bea67e42
Samuel Liew <samliew@gmail.com>
2018-10-13 00:17:11 +1100

    Release 1.0
    Respect API backoff param, pagination, relativetime format, spinner

--- RecentlyRejectedSuggestedEdits.user.js
+++ RecentlyRejectedSuggestedEdits.user.js
@@ -3,7 +3,7 @@
 // @description  New page to review recently rejected suggested edits
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      0.1
+// @version      1.0
 //
 // @include      https://*stackoverflow.com/review/suggested-edits*
 // @include      https://*serverfault.com/review/suggested-edits*
@@ -26,37 +26,99 @@
 
     const sitename = StackExchange.options.site.name.replace('Stack Exchange').trim();
     const resultsDiv = $(`<div id="reviews"></div>`);
-    const now = new Date();
+    const pagerDiv = $(`<div class="pager fl"></div>`);
+    let backoff = new Date();
 
 
     const getQueryParam = key => new URLSearchParams(location.search).get(key);
     const toDateFormat = d => d.toISOString().replace('T', ' ').replace('.000', '');
 
 
-    function getRejected(page = 1) {
-        resultsDiv.empty();
+    function buildPagination(max, page = 1, baseUrl = "", range = 5) {
 
-        $.get(`https://api.stackexchange.com/2.2/suggested-edits?pagesize=100&order=desc&sort=rejection&filter=!*KkBP6Je7loS9)xf&site=${location.hostname}`, function(data) {
+        pagerDiv.empty();
+
+        let str = '';
+        let from = page - range;
+        let to = page + range;
+
+        if(from < 1) from = 1;
+        if(to > max) to = max;
+
+        if(page - 1 >= 1) {
+            str += `<a href="${baseUrl}?page=${page - 1}" rel="next"><span class="page-numbers prev">prev</span></a> `;
+        }
+
+        if(from > 1) {
+            str += `<a href="${baseUrl}?page=1"><span class="page-numbers">1</span></a> `;
+            if(from > 2) {
+                str += `<span class="page-numbers dots">…</span> `;
+            }
+        }
+
+        for(let i = from; i <= to; i++) {
+            i == page ?
+              str += `<span class="page-numbers current">${i}</span> ` :
+              str += `<a href="${baseUrl}?page=${i}"><span class="page-numbers">${i}</span></a> `;
+        }
+
+        if(to < max) {
+            if(to < max - 1) {
+                str += `<span class="page-numbers dots">…</span> `;
+            }
+            str += `<a href="${baseUrl}?page=${max}"><span class="page-numbers">${max}</span></a> `;
+        }
+
+        if(page + 1 <= max) {
+            str += `<a href="${baseUrl}?page=${page + 1}" rel="next"><span class="page-numbers next">next</span></a> `;
+        }
+
+        pagerDiv.html(str);
+    }
+
+
+    function getRejected(page = 1) {
+
+        // Check for backoff
+        if(new Date() <= backoff) {
+            console.log('backoff');
+            return;
+        }
+
+        resultsDiv.empty();
+        StackExchange.helpers.addSpinner('#reviews');
+
+        $.get(`https://api.stackexchange.com/2.2/suggested-edits?page=${page}&pagesize=100&order=desc&sort=rejection&filter=!*KkBP6Je7loS9)xf&site=${location.hostname}`, function(data) {
+            StackExchange.helpers.removeSpinner();
+
+            // Store backoff value if found
+            if(data.backoff) {
+                backoff = new Date();
+                backoff.setSeconds( backoff.getSeconds() + data.backoff);
+            }
+
             const items = data.items;
             let html = '';
 
             items.forEach(function(v) {
+                const posttype = v.post_type.charAt(0);
                 const creationDate = new Date(v.creation_date * 1000);
                 const rejectionDate = new Date(v.rejection_date * 1000);
-                const proposingUser = v.proposing_user ? `<a href="${v.proposing_user.link}" title="rep:${v.proposing_user.reputation}, type:${v.proposing_user.user_type}">
-  <img src="${v.proposing_user.profile_image}" width="16" /> ${v.proposing_user.display_name}</a>` : 'anonymous';
+                const proposingUser = !v.proposing_user ? '<span class="userlink">anonymous</span>' :
+                  `<a class="userlink" href="${v.proposing_user.link}" title="rep: ${v.proposing_user.reputation}, type: ${v.proposing_user.user_type}">${v.proposing_user.display_name}</a>`;
 
                 html += `<div class="review">
   ${proposingUser}
   <a href="/suggested-edits/${v.suggested_edit_id}">suggested edit</a>
-  on <a href="/q/${v.post_id}" class="answer-hyperlink">${v.post_id}</a> (${v.post_type.charAt(0)})
-  <span title="${toDateFormat(creationDate)}" class="relativetime"></span>,
-  and rejected <span title="${toDateFormat(rejectionDate)}" class="relativetime"></span>
+  on <a href="/${posttype}/${v.post_id}" class="answer-hyperlink">${posttype}${v.post_id}</a>
+  was rejected <span title="-" class="relativetime">${toDateFormat(rejectionDate)}</span>
 </div>`;
             });
 
             resultsDiv.append(html);
             StackExchange.realtime.updateRelativeDates();
+
+            buildPagination(Math.ceil(data.total / data.page_size), data.page);
         });
     }
 
@@ -74,15 +136,33 @@
         <a href="/review/suggested-edits/history/rejects" class="youarehere">rejects</a>
         <a href="/review/suggested-edits">review</a>
     </div>
-</div>`).append(resultsDiv);
+</div>`).append(resultsDiv).append(pagerDiv);
 
         // Get list from API
         getRejected(1);
 
-        // Update timings every min
-        setInterval(function() {
+        // Update timings every 30 secs, doesn't work with toggle time format
+        //setInterval(function() {
+        //    StackExchange.realtime.updateRelativeDates();
+        //}, 30000);
+
+        // Handle same-page ajax pagination
+        pagerDiv.on('click', 'a', function(evt) {
+            const pagenum = Number($(this).attr('href').match(/page=\d+/)[0].split('=')[1]);
+            getRejected(pagenum);
+            return false;
+        });
+
+        // Toggle time format
+        $(`<a class="toggle-date-format">toggle format</a>`).insertBefore(resultsDiv).click(function() {
+            $('#reviews .relativetime').each(function(i, el) {
+                // Switch title and text
+                const tmp = el.innerText;
+                el.innerText = el.title;
+                el.title = tmp;
+            });
             StackExchange.realtime.updateRelativeDates();
-        }, 60000);
+        });
     }
 
 
@@ -108,7 +188,30 @@
 
         const styles = `
 <style>
-
+#reviews {
+    line-height: 2em;
+    font-size: 14px;
+}
+.review:nth-child(even) {
+    background: #f6f6f6;
+}
+.userlink {
+    display: inline-block;
+    min-width: 250px;
+    text-align: right;
+}
+.answer-hyperlink {
+    display: inline-block;
+    min-width: 71px;
+    margin-bottom: 0;
+}
+.relativetime {
+    display: inline-block;
+    min-width: 83px;
+}
+.relativetime:not([title$='Z']):before {
+    content: 'at ';
+}
 </style>
 `;
         $('body').append(styles);
