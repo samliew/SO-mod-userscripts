700f6b9c97d58b2c8a81c22c828f9eb0b704cc24
Samuel Liew <samliew@gmail.com>
2018-10-21 22:17:55 +1100

    Allow for use in Teams

--- RedactedScreenshots.user.js
+++ RedactedScreenshots.user.js
@@ -3,7 +3,7 @@
 // @description  Masks and hides user-identifing info
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.5
+// @version      1.5.1
 //
 // @include      https://*stackoverflow.com/*
 // @include      https://*serverfault.com/*
@@ -13,7 +13,6 @@
 // @include      https://*.stackexchange.com/*
 //
 // @exclude      *chat.*
-// @exclude      https://stackoverflow.com/c/*
 // ==/UserScript==
 
 (function() {
