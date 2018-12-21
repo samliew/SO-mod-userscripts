53c075172eaf9bee02c66c9502ba4c865a645558
Samuel Liew <samliew@gmail.com>
2018-12-21 13:45:36 +1100

    Update for new layout

--- FetchVoteCountsAgain.user.js
+++ FetchVoteCountsAgain.user.js
@@ -3,7 +3,7 @@
 // @description  Fetch vote counts for posts and enables you to click to fetch them again, even if you do not have sufficient rep. Also enables fetch vote counts on posts in mod flag queue.
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.2.3
+// @version      1.3
 //
 // @include      https://*stackoverflow.com/*
 // @include      https://*serverfault.com/*
@@ -25,15 +25,18 @@
 
         $('.vote-count-post').attr('title', 'View upvote and downvote totals');
 
-        $('#content').on('click', '.vote-count-post', function() {
+        $('#content').on('click', '.js-vote-count', function() {
             const votesElem = $(this);
             let pid = $(this).parents('.answer').first().attr('data-answerid');
             if(pid == null) pid = $(this).parents('.question').attr('data-questionid');
 
             // If user does not have vote counts priv, use API to fetch vote counts
             if(StackExchange.options.user.rep < 1000) {
+                StackExchange.helpers.addSpinner(this);
+
                 $.get(`https://api.stackexchange.com/2.2/posts/${pid}?filter=!w-*Ytm8Gt4I)pS_ZBu&site=${location.hostname}`)
                     .done(function(data) {
+                        StackExchange.helpers.removeSpinner();
                         votesElem.attr('title', `${+data.items[0].up_vote_count} up / ${+data.items[0].down_vote_count} down`)
                             .html(`<div style="color:green">${data.items[0].up_vote_count}</div><div class="vote-count-separator"></div><div style="color:maroon">${-data.items[0].down_vote_count}</div>`);
                     });
@@ -42,8 +45,11 @@
             // User has vote count priv and already fetched vote counts once
             //   or on mod page (mods can't fetch vote counts)
             else if((votesElem.children().length > 1 && StackExchange.options.user.rep >= 1000) || $('body').hasClass('mod-page')) {
+                StackExchange.helpers.addSpinner(this);
+
                 $.get(`https://${location.hostname}/posts/${pid}/vote-counts`)
                     .done(function(data) {
+                        StackExchange.helpers.removeSpinner();
                         votesElem.attr('title', `${+data.up} up / ${+data.down} down`)
                             .html(`<div style="color:green">${data.up}</div><div class="vote-count-separator"></div><div style="color:maroon">${data.down}</div>`);
                     });
@@ -58,7 +64,7 @@
 
         const styles = `
 <style>
-.votecell .vote-count-post {
+.votecell .js-vote-count {
     cursor: pointer !important;
 }
 </style>
