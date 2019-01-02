7c6d858dcad28da2327d063beca2bd0966d8fbfc
Samuel Liew <samliew@gmail.com>
2019-01-03 10:17:07 +1100

    Match font size in mod queue

--- FetchQuestionStats.user.js
+++ FetchQuestionStats.user.js
@@ -3,7 +3,7 @@
 // @description  Display number of comments on each post in question lists. For mod queues, additional info (recent revision history) is also retrieved.
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.3
+// @version      1.3.1
 //
 // @include      https://*stackoverflow.com/*
 // @include      https://*serverfault.com/*
@@ -167,6 +167,9 @@
 }
 .flagged-post-row .statscontainer a {
     color: inherit;
+}
+.flagged-post-row .statscontainer .views {
+    font-size: inherit;
 }
 .flagged-post-row .statscontainer .warning {
     color: #f00;
