c972bcadb8cb98f2ac0aab2b26bbf6ca216de4f2
Samuel Liew <samliew@gmail.com>
2019-01-04 19:53:13 +1100

    Refactor

--- UserActivityNotifications.user.js
+++ UserActivityNotifications.user.js
@@ -3,7 +3,7 @@
 // @description  Display notifications on user profile when new activity is detected since page load
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      0.1
+// @version      0.1.1
 //
 // @include      https://*stackoverflow.com/*
 // @include      https://*serverfault.com/*
@@ -17,48 +17,52 @@
 // ==/UserScript==
 
 
+// If user accepts, we can show native notifications
+Notification.requestPermission();
+
+
 (function() {
     'use strict';
 
 
-    Notification.requestPermission();
-
-
     const apikey = 'dhFaTnM59qx5gK807L7dNw((';
-    const siteTitle = StackExchange.options.site.name.replace(' Stack Exchange', '.SE');
     const pollInterval = 30;
     let lastCheckedDate = 0;
     let interval;
     let userId, username;
 
 
-    // Show notification
+    // Show native notification
     function notify(title, link, details = {}, dismissAfter = 15) {
         var n = new Notification(title, details);
+
+        // Open content if notification clicked
         if(typeof link !== 'undefined') {
-            n.onclick = function(event) {
-                event.preventDefault(); // prevent the browser from focusing the Notification's tab
+            n.onclick = function(evt) {
+                evt.preventDefault(); // prevent the browser from focusing the triggering Notification's tab
                 window.open(link, '_blank');
             }
         }
+
+        // Auto-dismiss notification
         if(dismissAfter >= 3) setTimeout(n.close.bind(n), dismissAfter * 1000);
     }
 
 
-    // Backoff from now
+    // Set a global backoff variable from now until X seconds later
     function addBackoff(secs) {
         if(isNaN(secs)) return;
         const w = window;
         w.backoff = setTimeout(() => { clearTimeout(w.backoff); w.backoff = null }, secs * 1000);
     }
+    // Helper method to check if backoff is active
     const hasBackoff = () => typeof backoff !== 'undefined' && !isNaN(backoff);
 
 
     // Get user info
     function getUserInfo(uid, fromdate = 0) {
         return new Promise(function(resolve, reject) {
-            if(typeof uid === 'undefined' || uid === null) { reject(); return; }
-            if(hasBackoff()) { reject(); return; }
+            if(typeof uid === 'undefined' || uid === null || hasBackoff()) { reject(); return; }
             $.get(`http://api.stackexchange.com/2.2/users/${uid}/timeline?pagesize=30&fromdate=${lastCheckedDate}&site=${location.hostname}&filter=!))yem8S&key=${apikey}`)
                 .done(function(data) {
                     lastCheckedDate = Math.floor(Date.now() / 1000);
@@ -78,13 +82,13 @@
                 let action = "";
                 switch(lastAction.timeline_type) {
                     case 'commented': action = 'commented'; break;
-                    case 'revision': action = 'edited a post'; break;
-                    case 'suggested': action = 'suggested an edit'; break;
+                    case 'revision': action = 'edited post'; break;
+                    case 'suggested': action = 'suggested edit'; break;
                     case 'reviewed': action = 'reviewed'; break;
-                    case 'answer': action = 'posted an answer'; break;
-                    case 'question': action = 'asked a question'; break;
-                    case 'accepted': action = 'accepted an answer'; break;
-                    case 'badge': action = 'earned a badge'; break;
+                    case 'answer': action = 'posted answer'; break;
+                    case 'question': action = 'asked question'; break;
+                    case 'accepted': action = 'accepted answer'; break;
+                    case 'badge': action = 'earned badge'; break;
                 }
                 notify(`${username} ${action}`, lastAction.link, {
                     body: `${lastAction.detail ? '"'+lastAction.detail+'"' : ''} on ${lastAction.title}`
@@ -103,21 +107,28 @@
         userId = Number(location.pathname.match(/\/\d+\//)[0].replace(/\D+/g, ''));
         username = $('.profile-user--name > div:first, .mini-avatar .name').text().trim().replace('♦', '').replace(/\s+/g, ' ');
 
-        // Load pnotify - https://sciactive.com/pnotify/
-        $.getScript('https://cdnjs.cloudflare.com/ajax/libs/pnotify/3.2.1/pnotify.js', function() {
-            console.log('pnotify loaded');
-        });
-
         // Run once on page load, then start polling API occasionally
         scheduledTask();
         interval = setInterval(scheduledTask, pollInterval * 1000);
     }
 
 
+    function loadPnotify() {
+
+        $('body').append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pnotify/3.2.1/pnotify.css" />');
+
+        // Load pnotify - https://sciactive.com/pnotify/
+        $.getScript('https://cdnjs.cloudflare.com/ajax/libs/pnotify/3.2.1/pnotify.js', function() {
+            console.log('pnotify loaded');
+
+            // TODO
+        });
+    }
+
+
     function appendStyles() {
 
         const styles = `
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pnotify/3.2.1/pnotify.css" />
 <style>
 </style>
 `;
