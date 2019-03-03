3f7a64636d0c429c9e8f4807d91d302be8f1310d
Samuel Liew <samliew@gmail.com>
2019-03-03 12:43:23 +1100

    Display inline for post header in new mod dashboard

--- ModUserQuicklinksEverywhere.user.js
+++ ModUserQuicklinksEverywhere.user.js
@@ -3,7 +3,7 @@
 // @description  Adds quicklinks to user infobox in posts
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      2.5
+// @version      2.6
 //
 // @include      https://*stackoverflow.com/*
 // @include      https://*serverfault.com/*
@@ -28,7 +28,7 @@
 
 
     function addUserLinks() {
-        $('.post-user-info, .user-details')
+        $('.post-user-info, .user-details, .js-flagged-post .js-post-header .js-hide-on-delete > div > div.ai-center.fw-wrap')
             .not('[js-mod-quicklinks]')
             .attr('js-mod-quicklinks', 'true')
             .find('a[href^="/users/"]:first').each(function() {
@@ -36,7 +36,7 @@
                 // Add Votes and IP-xref links after mod-flair if mod, or after the user link
                 const uid = this.href.match(/\d+/);
                 const modFlair = $(this).next('.mod-flair');
-                const userlinks = $(`<div class="mod-userlinks">[
+                const userlinks = $(`<div class="mod-userlinks grid--cell">[
   <a href="${parentUrl}/users/account-info/${uid}" target="_blank">mod</a>
 | <a href="${parentUrl}/admin/show-user-votes/${uid}" target="_blank">votes</a>
 | <a href="${parentUrl}/admin/xref-user-ips/${uid}?daysback=30&threshold=2" target="_blank">xref</a>
@@ -86,6 +86,10 @@
 }
 .deleted-answer .mod-userlinks {
     background-color: #f4eaea;
+}
+.grid--cell + .mod-userlinks {
+    display: inline-block;
+    width: auto;
 }
 /* review stats/leaderboard */
 .stats-mainbar .task-stat-leaderboard .user-details {
