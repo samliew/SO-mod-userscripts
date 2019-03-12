0e6ea08098859d909c26a3542de3a6ef0bd36cd9
Samuel Liew <samliew@gmail.com>
2019-03-12 11:05:49 +1100

    Now works with Chat!

--- UserInfoSidebar.user.js
+++ UserInfoSidebar.user.js
@@ -3,7 +3,7 @@
 // @description  Adds user moderation links sidebar with quicklinks & user details (from Mod Dashboard) to user-specific pages
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.2
+// @version      2.0
 //
 // @include      https://*stackoverflow.com/*
 // @include      https://*serverfault.com/*
@@ -12,37 +12,124 @@
 // @include      https://*mathoverflow.net/*
 // @include      https://*.stackexchange.com/*
 //
-// @exclude      *chat.*
+// @include      https://chat.stackexchange.com/*
+// @include      https://chat.meta.stackexchange.com/*
+// @include      https://chat.stackoverflow.com/*
+//
 // @exclude      https://stackoverflow.com/c/*
+//
+// @require      https://github.com/samliew/SO-mod-userscripts/raw/master/lib/common.js
+//
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     // Moderator check
-    if(typeof StackExchange == "undefined" || !StackExchange.options || !StackExchange.options.user || !StackExchange.options.user.isModerator ) return;
+    if(!isModerator()) return;
+
+
+    const isChat = location.hostname.includes('chat.');
+
+
+    function getChatParentUser() {
+        const parentuser = $('.user-stats a').last().attr('href');
+        return parentuser ? 'https:' + parentuser : '';
+    }
 
 
     function getCurrentUserId() {
-        if(location.pathname.indexOf('/users/message/') === 0 || location.pathname.indexOf('/admin/cm-message/') === 0) {
+
+        // Mod & CM messages
+        if(location.pathname.includes('/users/message/') || location.pathname.includes('/admin/cm-message/')) {
             return $('.msg-moderator:first a[href^="/users/"], #addressing .user-details a').last().attr('href').match(/\d+/)[0];
         }
+
+        // User & user admin pages
         if(document.body.classList.contains('user-page') || (/[/-]users?[/-]/.test(location.href)) && document.body.classList.contains('mod-page')) {
             return location.href.match(/\d+/)[0];
         }
 
+        // Chat
+        if(isChat && location.pathname.includes('/users/')) {
+            const parentuser = getChatParentUser();
+            return parentuser ? parentuser.match(/\d+/)[0] : null;
+        }
+
+        // Question asker
         const questionUser = $('#question .user-info:last a').first();
         if(questionUser.length !== 0) {
             return questionUser.attr('href').match(/\d+/)[0];
         }
+
+        // Default
         return null;
     }
 
 
     function doPageload() {
         const uid = getCurrentUserId();
-        if(uid) {
-            //console.log(`Current User: ${uid}`);
+        console.log(`Current User: ${uid}`);
+
+        if(!uid) return;
+
+        if(isChat) {
+
+            const userModPage = getChatParentUser().replace('/users/', '/users/account-info/').replace(/\D+$/, '');
+            const mainSiteHostname = userModPage.split('/users/')[0];
+
+            ajaxPromise(userModPage, 'document').then(function(data) {
+                const username = $('h1', data).first().get(0).childNodes[0].nodeValue.trim();
+
+                // Modify quicklinks and user details, then append to page
+                const $quicklinks = $('div.mod-links', data).attr('id', 'usersidebar');
+                const $modActions = $quicklinks.find('.mod-actions');
+
+                // Move contact links
+                $modActions.find('li').slice(-4, -2).appendTo($quicklinks.find('ul:first'));
+
+                // Remove other actions as they need additional work to get popup working
+                $modActions.last().remove();
+
+                // Headers
+                const $infoHeader = $quicklinks.find('h3').last().text(username).prependTo($quicklinks);
+
+                // Insert user details
+                const $info = $('.mod-section .details', data).insertAfter($infoHeader);
+                $info.children('.row').each(function() {
+                    $(this).children().first().unwrap();
+                });
+
+                // Transform user details to list format
+                $info.children('.col-2').removeClass('col-2').addClass('info-header');
+                $info.children('.col-4').removeClass('col-4').addClass('info-value');
+
+                // Change xref link to month to be more useful (default was week)
+                $quicklinks.find('a[href*="xref-user-ips"]').attr('href', (i, v) => v += '?daysback=30&threshold=2');
+
+                // Since we are on chat, transform links to main links
+                $('a[href^="/"]', $info).attr('href', (i, v) => mainSiteHostname + v);
+                $('.mod-quick-links a', $quicklinks).attr('href', (i, v) => mainSiteHostname + v);
+
+                // Check if user is currently suspended, highlight username
+                const susMsg = $('.system-alert', data).first().text();
+                if(susMsg.indexOf('suspended') >= 0) {
+                    const susDur = susMsg.split('ends')[1].replace(/(^\s|(\s|\.)+$)/g, '');
+                    $quicklinks.find('h3').first().attr({ style: 'color: red !important;' }).attr('title', `currently suspended (ends ${susDur})`);
+                }
+
+                // Append to page
+                $('body').append($quicklinks);
+            });
+
+            // Handle resize
+            $(window).on('load resize', function() {
+                $('body').toggleClass('usersidebar-open', $(document).width() >= 1400);
+            });
+
+        }
+        else {
 
             // Get user's mod dashboard page
             $.get('/users/account-info/' + uid, function(data) {
@@ -71,10 +158,7 @@
                 $info.children('.col-2').removeClass('col-2').addClass('info-header');
                 $info.children('.col-4').removeClass('col-4').addClass('info-value');
 
-                // Add history link to quicklinks, so you don't have to open the mod popup and switch tabs
-                $('.mod-quick-links', $quicklinks).prepend(`<li><a href="/users/history/${uid}">history</a></li>`);
-
-                // Add history link to quicklinks, so you don't have to open the mod popup and switch tabs
+                // Change xref link to month to be more useful (default was week)
                 $quicklinks.find('a[href*="xref-user-ips"]').attr('href', (i, v) => v += '?daysback=30&threshold=2');
 
                 // If on meta,
@@ -96,15 +180,13 @@
                 // Append to page
                 $('body').append($quicklinks);
             });
+
+            // Handle resize
+            $(window).on('load resize', function() {
+                $('body').toggleClass('usersidebar-open', $(document).width() >= 1660);
+                $('body').toggleClass('usersidebar-compact', $(window).height() <= 680);
+            });
         }
-    }
-
-
-    function listenToPageUpdates() {
-        $(window).on('load resize', function() {
-            $('body').toggleClass('usersidebar-open', $(document).width() >= 1660);
-            $('body').toggleClass('usersidebar-compact', $(window).height() <= 680);
-        });
     }
 
 
@@ -112,6 +194,25 @@
 
         const styles = `
 <style>
+/* copied from main site as chat doesn't have this style */
+.mod-links li {
+    margin-bottom: 5px;
+}
+.bounty-indicator-tab {
+    color: #FFF !important;
+    display: inline;
+    background-color: #0077dd;
+    padding: .2em .5em .25em;
+    margin-right: 5px;
+    font-size: 10px;
+    line-height: 1.3;
+    border-radius: 2px;
+}
+
+
+#usersidebar * {
+    box-sizing: border-box;
+}
 #usersidebar {
     position: fixed;
     z-index: 8950;
@@ -137,6 +238,7 @@
     border: 1px solid #ccc;
     border-left: none;
     box-shadow: 3px 2px 10px -2px rgba(0,0,0,0.25);
+    box-sizing: border-box;
 }
 .usersidebar-open #usersidebar,
 #usersidebar:hover {
@@ -190,6 +292,5 @@
     // On page load
     appendStyles();
     doPageload();
-    listenToPageUpdates();
 
 })();
