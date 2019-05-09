c3c4183f7654287995d32672abda5c377d0eaf34
Samuel Liew <samliew@gmail.com>
2019-05-09 15:55:20 +1000

    Update UserReviewBanHelper.user.js

--- UserReviewBanHelper.user.js
+++ UserReviewBanHelper.user.js
@@ -3,7 +3,7 @@
 // @description  Display users' prior review bans in review, Insert review ban button in user review ban history page, Load ban form for user if user ID passed via hash
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      2.3
+// @version      3.0-dev
 //
 // @include      */review/close*
 // @include      */review/reopen*
@@ -19,6 +19,8 @@
 //
 // @include      */admin/review/audits*
 // @include      */admin/review/bans*
+//
+// @require      https://github.com/samliew/SO-mod-userscripts/raw/master/lib/common.js
 // ==/UserScript==
 
 (function() {
@@ -26,28 +28,29 @@
 
 
     // Moderator check
-    if(typeof StackExchange == "undefined" || !StackExchange.options || !StackExchange.options.user || !StackExchange.options.user.isModerator ) return;
+    if(!isModerator()) return;
 
 
     const fkey = StackExchange.options.user.fkey;
+    const messageCharLimit = 1000;
+
     const defaultBanMessage = `Your recent [reviews](https://${location.hostname}/users/current?tab=activity&sort=reviews) wasn't helpful. Please review the history of the posts and consider how choosing a different action would help achieve those outcomes more quickly.`;
     const permaBanMessage = `Due to your [poor review history](https://${location.hostname}/users/current?tab=activity&sort=reviews) as well as no signs of improvement after multiple review bans, you are no longer welcome to use any review queues on the site.`;
+
+    const cannedMessages = {
+        postNaa: `You recently reviewed [this QUEUENAME post](POSTLINK). Although it was posted as an answer, it clearly did not attempt to provide an answer to the question. You should have flagged it as "not an answer" so that it could be removed.`,
+        postNaaEdited: `You recently edited [this QUEUENAME post](POSTLINK). Please do not edit posts that should have been deleted. Use "edit" only when your edit salvages the post and makes it a valid answer.`,
+        postNaaCommentOnly: `You recently reviewed [this QUEUENAME post](POSTLINK). Although you correctly identified it as not being an answer, you chose to leave a comment. That did not help to solve the problem. You should have flagged it as "not an answer" so that it could be removed.`,
+        postLinkOnly: `You recently reviewed [this post](POSTLINK). It contained nothing more than a link to an off-site resource, which does not meet our minimum standards for an answer (https://stackoverflow.com/help/how-to-answer). You should have flagged it as "not an answer" or "very low quality" so that it could be removed. Please read [How should I get started reviewing Late Answers and First Posts?](https://meta.stackoverflow.com/q/288505)`,
+        postEditPlagiarism: `You reviewed [this post](POSTLINK) incorrectly. The suggested edit was for the most part, plagiarism. Please pay more attention to each review in the future.`,
+    };
 
 
     const reloadPage = () => location.reload(true);
     const pluralize = s => s.length != 0 ? 's' : '';
 
-
-    // Solution from https://stackoverflow.com/a/24719409/584192
-    function jQueryXhrOverride() {
-        var xhr = jQuery.ajaxSettings.xhr();
-        var setRequestHeader = xhr.setRequestHeader;
-        xhr.setRequestHeader = function(name, value) {
-            if (name == 'X-Requested-With') return;
-            setRequestHeader.call(this, name, value);
-        };
-        return xhr;
-    }
+    // For review ban message
+    let params, uid, posts, posttext = '';
 
 
     // Review unban user
@@ -175,64 +178,54 @@
                 return false;
             });
 
-            // Load ban form for user if passed via querystring
-            var params = location.hash.substr(1).split('|');
-            var uid = params[0];
-            var posts = params[1].split(';').map(v => v.replace(/\/?review\//, ''));
+            // UI classes
+            $('#user-to-ban').addClass('s-input');
+            $('#lookup').addClass('s-btn');
 
-            // Remove similar consecutive review types from urls
-            var prevType = null;
-            posts = posts.map(v => {
-                if(v.includes(prevType + '/')) v = v.replace(/\D+/g, '');
-                else prevType = v.split('/')[0];
-                return v;
-            });
-            console.log(posts);
-
-            // Fit as many URLs as possible into message
-            var posttext = '';
-            var i = posts.length;
-            do {
-                posttext = posts.slice(0, i--).join(", ");
-            }
-            while(i > 1 && 48 + location.hostname.length + posttext.length > 300);
-
-            // Completed loading lookup
-            $(document).ajaxComplete(function(event, xhr, settings) {
-                if(settings.url.includes('/admin/review/lookup-bannable-user')) {
-
-                    // Insert ban message if review link found
-                    if(typeof params[1] !== 'undefined') {
-                        var banMsg = `Your review${pluralize(posts)} on https://${location.hostname}/review/${posttext} wasn't helpful.`;
-                        if(banMsg.length < 300 - 102) banMsg += ` Do review the history of the post${pluralize(posts)} and consider which action would achieve that outcome more quickly.`;
-                        $('textarea[name=explanation]').val(banMsg);
-                    }
-
-                    // Change default to other
-                    $('#days-other').click();
-
-                    $('#days-3').val('4').next('label').text('4 days for first review ban');
-                    $('#days-7').val('8').next('label').text('8 days for second review ban');
-                    $('#days-30').val('16').next('label').text('16 days for third review ban');
-                    $('#days-other')
-                        .before(`<input type="radio" value="32" name="reviewBanChoice" id="days-32"><label for="days-32"> 32 days for fourth review ban</label><br>`)
-                        .before(`<input type="radio" value="64" name="reviewBanChoice" id="days-64"><label for="days-64"> 64 days for fifth review ban</label><br>`)
-                        .before(`<input type="radio" value="128" name="reviewBanChoice" id="days-128"><label for="days-128"> 128 days for subsequent review bans</label><br>`);
-
-                    // Run once only
-                    $(event.currentTarget).unbind('ajaxComplete');
+            // If attempt to submit ban form without selecting duration, alert and prevent
+            $('#lookup-result').on('submit', 'form', function() {
+                if($('input[name="reviewBanChoice"]:checked').length == 0) {
+                    $(this).addClass('validation-error');
+                    return false;
                 }
             });
 
-            // Validation
-            if(/\d+/.test(uid)) {
+            // Load ban form for user if passed via querystring
+            params = location.hash.substr(1).split('|');
+            if(params) {
+                uid = params[0];
 
-                // Insert UID
-                $('#user-to-ban').val(uid);
+                if(params.length === 2) {
+                    posts = params[1].split(';').map(v => v.replace(/\/?review\//, ''));
 
-                // Submit lookup
-                setTimeout(() => $('#lookup').click(), 500);
+                    // Remove similar consecutive review types from urls
+                    var prevType = null;
+                    posts = posts.map(v => {
+                        if(v.includes(prevType + '/')) v = v.replace(/\D+/g, '');
+                        else prevType = v.split('/')[0];
+                        return v;
+                    });
+                    console.log(posts);
+
+                    // Fit as many URLs as possible into message
+                    var i = posts.length;
+                    do {
+                        posttext = posts.slice(0, i--).join(", ");
+                    }
+                    while(i > 1 && 48 + location.hostname.length + posttext.length > messageCharLimit);
+                }
+
+                // Validation
+                if(/\d+/.test(uid)) {
+
+                    // Insert UID
+                    $('#user-to-ban').val(uid);
+
+                    // Submit lookup
+                    setTimeout(() => $('#lookup').click(), 200);
+                }
             }
+
         }
         // Mod user history - review bans filter
         else if(location.pathname.includes('/users/history') && location.search == "?type=User+has+been+banned+from+review") {
@@ -313,6 +306,55 @@
     }
 
 
+    function listenForPageUpdates() {
+
+        // Completed loading lookup
+        $(document).ajaxComplete(function(event, xhr, settings) {
+            if(settings.url.includes('/admin/review/lookup-bannable-user')) {
+
+                // Insert ban message if review link found
+                if(typeof posts !== 'undefined') {
+                    var banMsg = `Your review${pluralize(posts)} on https://${location.hostname}/review/${posttext} wasn't helpful.`;
+                    if(banMsg.length < messageCharLimit - 102) banMsg += ` Do review the history of the post${pluralize(posts)} and consider which action would achieve that outcome more quickly.`;
+                    $('textarea[name=explanation]').val(banMsg);
+                }
+
+                // Update message label
+                $('label[for="explanation"]').html(`Explain why this person is being banned; it will be shown to them when they try to review. Comment markdown supported.<div>Example:</div>`)
+                    .after(`<div class="examples"><pre>You approved edits on [blatant spam](&lt;link to review task&gt;)</pre></div>`);
+
+                // Update message max length
+                $('textarea[name="explanation"]').addClass('s-textarea').attr('maxlength', messageCharLimit).wrapAll(`<div class="message-wrapper"></div>`);
+
+                // TODO: Add canned messages
+                const cans = $(`<div id="canned-messages"></div>`).appendTo('.message-wrapper');
+
+
+                // Duration radios
+                $('#days-3').val('2').next('label').text('2 days');
+                $('#days-7').val('4').next('label').text('4 days');
+                $('#days-30').val('8').next('label').text('8 days');
+                $('#days-other')
+                    .before(`<input type="radio" value="16" name="reviewBanChoice" id="days-16"><label for="days-16"> 16 days</label><br>`)
+                    .before(`<input type="radio" value="32" name="reviewBanChoice" id="days-32"><label for="days-32"> 32 days</label><br>`)
+                    .before(`<input type="radio" value="64" name="reviewBanChoice" id="days-64"><label for="days-64"> 64 days </label><br>`)
+                    .before(`<input type="radio" value="128" name="reviewBanChoice" id="days-128"><label for="days-128"> 128 days</label><br>`)
+                    .before(`<input type="radio" value="256" name="reviewBanChoice" id="days-256"><label for="days-256"> 256 days</label><br>`)
+                    .before(`<input type="radio" value="365" name="reviewBanChoice" id="days-365"><label for="days-365"> 365 days</label><br>`)
+                    .before(`<div class="duration-error">Please select ban duration!</div>`)
+                    .next().addBack().remove();
+
+                // TODO: Default would be based on previous ban duration
+
+
+                // UI
+                $('#days-3').parent().addClass('duration-radio-group').find('input').addClass('s-radio');
+                $('#lookup-result input:submit').addClass('s-btn s-btn__primary');
+            }
+        });
+    }
+
+
     function appendStyles() {
 
         const styles = `
@@ -346,19 +388,73 @@ a.reviewban-button {
     float: right;
 }
 
-#lookup-result textarea {
-    min-height: 90px;
-    width: 100% !important;
-    margin-bottom: 10px;
-    font-family: monospace;
+#lookup-result {
+    margin-top: 20px !important;
 }
-#lookup-result input[name="reviewBanDays"] {
+#lookup-result .examples pre {
+    margin-bottom: 0;
+    padding: 5px 10px;
+}
+#lookup-result br {
+    display: none;
+}
+#lookup-result .duration-radio-group {
+    display: block;
+    width: 300px;
+    margin-bottom: 10px;
+}
+#lookup-result .duration-radio-group label {
+    margin-left: 2px;
+    cursor: pointer;
+}
+#lookup-result .duration-radio-group label:after {
+    content: '';
+    display: block;
+    margin-bottom: 4px;
+}
+#lookup-result .duration-radio-group input[name="reviewBanDays"] {
     position: absolute;
     margin-left: 10px;
     margin-top: -6px;
 }
+#lookup-result .duration-error {
+    display: none;
+    margin: 0;
+    color: red;
+}
+#lookup-result form.validation-error .duration-error {
+    display: block;
+}
 #lookup-result form > div {
     margin: 5px 0;
+}
+
+.message-wrapper {
+    position: relative;
+}
+.message-wrapper textarea {
+    display: block;
+    min-height: 120px;
+    max-width: none !important;
+    width: 80% !important;
+    margin-bottom: 20px;
+    font-family: monospace;
+}
+#canned-messages {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    width: 20%;
+    padding: 12px 15px;
+    border: 1px solid #ccc;
+    border-left: none;
+}
+#canned-messages:before {
+    content: 'Canned messages';
+    display: block;
+    margin-bottom: 5px;
+    font-weight: bold;
 }
 
 table.sorter > tbody > tr:nth-child(odd) > td {
@@ -399,5 +495,6 @@ a.reban:hover {
     // On page load
     appendStyles();
     doPageload();
+    listenForPageUpdates();
 
 })();
