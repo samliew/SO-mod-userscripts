8938fb19c62a6722ed35a0fe9d559ef834e8075d
Samuel Liew <samliew@gmail.com>
2019-06-12 12:59:27 +1000

    Bump major version to override rename update

--- StackExchangeDarkMode.user.js
+++ StackExchangeDarkMode.user.js
@@ -3,7 +3,7 @@
 // @description  Dark theme for sites and chat on the Stack Exchange Network
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.8.4
+// @version      2.0
 //
 // @include      https://*stackexchange.com/*
 // @include      https://*stackoverflow.com/*
