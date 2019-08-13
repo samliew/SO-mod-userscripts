2b7de33b410e16f2a0e8e1c448e0b89e3ab5a820
Samuel Liew <samliew@gmail.com>
2019-08-13 11:25:39 +1000

    Update ChatImprovements.user.js

--- ChatImprovements.user.js
+++ ChatImprovements.user.js
@@ -3,7 +3,7 @@
 // @description  Show users in room as a list with usernames, more timestamps
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      0.9.3
+// @version      0.9.4
 //
 // @include      https://chat.stackoverflow.com/*
 // @include      https://chat.stackexchange.com/*
@@ -202,7 +202,7 @@
             if((el.href.includes('/questions/') || el.href.includes('/q/') || el.href.includes('/a/')) && el.innerText.includes('…')) {
 
                 // Avoid truncating inline question links
-                el.innerText = el.href.replace('/questions/', '/q/').replace(/(\/\D*)+(\/\d+#comment\d+_\d+)?$/, '') +
+                el.innerText = el.href.replace('/questions/', '/q/').replace(/(\/\D*)+((\/\d+)?#comment\d+_\d+)?$/, '') +
                     (el.href.includes('#comment') ? el.href.match(/(#comment\d+)_\d+$/)[1] : '');
 
                 // Remove user id
