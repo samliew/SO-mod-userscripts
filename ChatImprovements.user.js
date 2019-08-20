e4dc2cd2c630708d36789ffb05af42a2649301d9
Samuel Liew <samliew@gmail.com>
2019-08-20 10:52:08 +1000

    Some mod diamonds are truncated in tiny sigs

--- ChatImprovements.user.js
+++ ChatImprovements.user.js
@@ -3,7 +3,7 @@
 // @description  Show users in room as a list with usernames, more timestamps, tiny avatars only, timestamps on every message, message parser, collapse room description and room tags, wider search box, mods with diamonds
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.0.2
+// @version      1.0.3
 //
 // @include      https://chat.stackoverflow.com/*
 // @include      https://chat.stackexchange.com/*
@@ -774,6 +774,7 @@ a.nowrap {
     #chat-body .signature .username.moderator:after {
         content: '♦';
         position: absolute;
+        top: 0;
         right: 0;
         font-size: 1.2em;
     }
