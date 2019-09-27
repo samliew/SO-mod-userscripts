31a3f640fefc52e391f786b8416db2b685833141
Samuel Liew <samliew@gmail.com>
2019-09-27 23:20:26 +1000

    Change CDN, perf improvements

--- ChatEmojis.user.js
+++ ChatEmojis.user.js
@@ -3,7 +3,7 @@
 // @description  Allows users to insert emojis into chat. If chat message contains just an emoji, increase display size
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.2
+// @version      1.3
 //
 // @include      https://chat.stackoverflow.com/rooms/*
 // @include      https://chat.stackexchange.com/rooms/*
@@ -35,7 +35,7 @@
 
     // Emoji regex - https://stackoverflow.com/a/41164587
     const emojiRegex = /^(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])$/;
-    const findEmojiMessages = () => $('.message .content').not('.msg-emoji').filter((i,v) => v.textContent.length <= 2 && emojiRegex.test(v.textContent || '')).addClass('msg-emoji');
+    const findEmojiMessages = () => $('.message .content').not('.js-emoji-parsed').addClass('js-emoji-parsed').filter((i,v) => v.textContent.length <= 2 && emojiRegex.test(v.textContent || '')).addClass('msg-emoji');
 
 
     function initEmojiPicker() {
@@ -43,7 +43,7 @@
         const chatfield = $('#input');
 
         // Load emojionearea - https://github.com/mervick/emojionearea/tree/version2
-        const liburl = 'https://cdn.rawgit.com/mervick/emojionearea/version2';
+        const liburl = 'https://cdn.jsdelivr.net/gh/mervick/emojionearea@2';
         $(document.body).append(`<link rel="stylesheet" href="${liburl}/css/emojionearea.min.css" />`);
 
         $.getCachedScript(`${liburl}/js/emojionearea.min.js`, function() {
@@ -138,7 +138,7 @@
             initEmojiPicker();
 
             // Occasionally, look for new single-emoji messages and apply class
-            setInterval(findEmojiMessages, 2000);
+            setInterval(findEmojiMessages, 1000);
         }
 
         findEmojiMessages();
