5f1500a7164ea36821aea701359e536352f84390
Samuel Liew <samliew@gmail.com>
2019-10-02 10:49:09 +1000

    No line breaks in description

--- ChatImprovements.user.js
+++ ChatImprovements.user.js
@@ -1,11 +1,9 @@
 // ==UserScript==
 // @name         Chat Improvements
-// @description  New responsive userlist with usernames and total count, more timestamps, use small signatures only, mods with diamonds,
-//               message parser (smart links), timestamps on every message, collapse room description and room tags, mobile improvements,
-//               expand starred messages on hover, highlight occurances of same user link, room owner changelog, pretty print styles, and more...
+// @description  New responsive userlist with usernames and total count, more timestamps, use small signatures only, mods with diamonds, message parser (smart links), timestamps on every message, collapse room description and room tags, mobile improvements, expand starred messages on hover, highlight occurances of same user link, room owner changelog, pretty print styles, and more...
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.9.3
+// @version      1.9.4
 //
 // @include      https://chat.stackoverflow.com/*
 // @include      https://chat.stackexchange.com/*
