65188f39e5da33236cbb83a6fff6fd82c55537f6
Samuel Liew <samliew@gmail.com>
2019-10-03 11:10:55 +1000

    Exclude topbar from print

--- ChatImprovements.user.js
+++ ChatImprovements.user.js
@@ -3,7 +3,7 @@
 // @description  New responsive userlist with usernames and total count, more timestamps, use small signatures only, mods with diamonds, message parser (smart links), timestamps on every message, collapse room description and room tags, mobile improvements, expand starred messages on hover, highlight occurances of same user link, room owner changelog, pretty print styles, and more...
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      2.1.3
+// @version      2.1.4
 //
 // @include      https://chat.stackoverflow.com/*
 // @include      https://chat.stackexchange.com/*
@@ -1621,6 +1621,8 @@ body.outside .access-section h2 {
     }
 
     body > span[style*="absolute"],
+    #topbar,
+    .topbar,
     #feed-ticker,
     #bottom,
     #input-area,
