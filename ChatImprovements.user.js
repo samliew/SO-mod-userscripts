955c171e98613e7a503c06d49478c740fb955fc6
Samuel Liew <samliew@gmail.com>
2019-10-09 12:14:28 +1100

    Username signature size

--- ChatImprovements.user.js
+++ ChatImprovements.user.js
@@ -3,7 +3,7 @@
 // @description  New responsive userlist with usernames and total count, more timestamps, use small signatures only, mods with diamonds, message parser (smart links), timestamps on every message, collapse room description and room tags, mobile improvements, expand starred messages on hover, highlight occurances of same user link, room owner changelog, pretty print styles, and more...
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      2.5
+// @version      2.5.1
 //
 // @include      https://chat.stackoverflow.com/*
 // @include      https://chat.stackexchange.com/*
@@ -1515,6 +1515,9 @@ ul#my-rooms > li > a span {
 }
 #present-users-list .inactive.js-user-highlight {
     opacity: 1 !important;
+}
+.monologue .tiny-signature .username {
+    display: inline;
 }
 
 
