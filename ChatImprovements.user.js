66cce11ae3fdb00da43ee36d5296d3c0f759d29f
Samuel Liew <samliew@gmail.com>
2019-10-23 09:14:55 +1100

    Hide dialogs and clear highlights on ESC

--- ChatImprovements.user.js
+++ ChatImprovements.user.js
@@ -3,7 +3,7 @@
 // @description  New responsive userlist with usernames and total count, more timestamps, use small signatures only, mods with diamonds, message parser (smart links), timestamps on every message, collapse room description and room tags, mobile improvements, expand starred messages on hover, highlight occurances of same user link, room owner changelog, pretty print styles, and more...
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      2.6.6
+// @version      2.7
 //
 // @include      https://chat.stackoverflow.com/*
 // @include      https://chat.stackexchange.com/*
@@ -970,9 +970,19 @@ a.topbar-icon.topbar-icon-on .topbar-dialog,
                 sites.show();
             }
         });
-        // Hide dialogs when clicking elsewhere
-        $('#main, #sidebar, #container').on('click', function() {
+
+        function closeTopbarDialogs() {
             $('#topbar .topbar-icon').removeClass('topbar-icon-on icon-site-switcher-on');
+        }
+        // Hide dialogs when clicking elsewhere
+        $('#main, #sidebar, #container').on('click', closeTopbarDialogs);
+        // Hide dialogs when pressing esc
+        $(document).on('keyup', function(evt) {
+            if(evt.keyCode == 27) {
+                closeTopbarDialogs();
+                $('#searchbox').val(''); // clear searchbox
+                $('.highlight').removeClass('highlight'); // clear highlighted messages
+            }
         });
 
 
