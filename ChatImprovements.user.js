ef987979d2be9b24ec77c41a2f563feb41f7414f
Samuel Liew <samliew@gmail.com>
2019-10-29 16:01:44 +1100

    Minor styles fix

--- ChatImprovements.user.js
+++ ChatImprovements.user.js
@@ -3,7 +3,7 @@
 // @description  New responsive userlist with usernames and total count, more timestamps, use small signatures only, mods with diamonds, message parser (smart links), timestamps on every message, collapse room description and room tags, mobile improvements, expand starred messages on hover, highlight occurances of same user link, room owner changelog, pretty print styles, and more...
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      2.9
+// @version      2.9.1
 //
 // @include      https://chat.stackoverflow.com/*
 // @include      https://chat.stackexchange.com/*
@@ -2017,7 +2017,7 @@ div.dialog-message > .meta {
 }
 .pager {
     margin: 0;
-    padding: 20px 0 10px;
+    padding: 30px 0;
     text-align: center;
     clear: both !important;
 }
