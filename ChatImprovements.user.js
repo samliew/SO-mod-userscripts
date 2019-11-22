dc590ed3cc7835f63b491b40ad067882b8a97989
Samuel Liew <samliew@gmail.com>
2019-11-22 20:03:23 +1100

    #51 Remove bgcolor for chat input area

--- ChatImprovements.user.js
+++ ChatImprovements.user.js
@@ -3,7 +3,7 @@
 // @description  New responsive userlist with usernames and total count, more timestamps, use small signatures only, mods with diamonds, message parser (smart links), timestamps on every message, collapse room description and room tags, mobile improvements, expand starred messages on hover, highlight occurances of same user link, room owner changelog, pretty print styles, and more...
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      2.10
+// @version      2.10.1
 //
 // @include      https://chat.stackoverflow.com/*
 // @include      https://chat.stackexchange.com/*
@@ -1562,7 +1562,6 @@ html.fixed-header body.with-footer main {
 }
 #input-area {
     height: 100px;
-    background-color: #eee;
 }
 #input {
     height: 88px;
