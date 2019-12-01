0cf6ca8c4bcec24ca096c7d3af43877bb097b788
Samuel Liew <samliew@gmail.com>
2019-12-01 22:52:28 +1100

    #52 - Remove timestamp color override

--- ChatImprovements.user.js
+++ ChatImprovements.user.js
@@ -3,7 +3,7 @@
 // @description  New responsive userlist with usernames and total count, more timestamps, use small signatures only, mods with diamonds, message parser (smart links), timestamps on every message, collapse room description and room tags, mobile improvements, expand starred messages on hover, highlight occurances of same user link, room owner changelog, pretty print styles, and more...
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      2.11
+// @version      2.11.1
 //
 // @include      https://chat.stackoverflow.com/*
 // @include      https://chat.stackexchange.com/*
@@ -1635,9 +1635,6 @@ ul#my-rooms > li > a span {
 }
 .monologue {
     min-width: 300px;
-}
-.monologue .timestamp {
-    color: #444;
 }
 .message a i.transcript-link {
     opacity: 0.5;
