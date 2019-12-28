eaddbb09d55ffe6f8cb08234bafb3349c866ec1a
Samuel Liew <samliew@gmail.com>
2019-12-28 10:19:21 +0800

    Bugfix

--- StackPrintStyles.user.js
+++ StackPrintStyles.user.js
@@ -3,7 +3,7 @@
 // @description  Print preprocessor and print styles for Stack Exchange Q&A, blog, and chat. Includes a handy load all comments button at bottom right.
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      0.2.5
+// @version      0.2.6
 //
 // @include      https://*stackexchange.com/*
 // @include      https://*stackoverflow.com/*
@@ -322,6 +322,11 @@
         width: calc(100% - 26px);
         margin: 0;
         padding: 0;
+    }
+    .monologue,
+    .system-message-container {
+        padding-top: 15px;
+        margin-bottom: -15px;
     }
     .monologue .signature {
         flex: 0 1 120px;
