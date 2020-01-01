878d5235833ca539b9b06a4ccbefc83f7940e3e2
Samuel Liew <samliew@gmail.com>
2020-01-01 17:03:16 +0800

    Bugfix

--- ChatImprovements.user.js
+++ ChatImprovements.user.js
@@ -3,7 +3,7 @@
 // @description  New responsive userlist with usernames and total count, more timestamps, use small signatures only, mods with diamonds, message parser (smart links), timestamps on every message, collapse room description and room tags, mobile improvements, expand starred messages on hover, highlight occurances of same user link, room owner changelog, pretty print styles, and more...
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      2.11.6
+// @version      2.11.7
 //
 // @include      https://chat.stackoverflow.com/*
 // @include      https://chat.stackexchange.com/*
@@ -1190,7 +1190,6 @@ a.topbar-icon.topbar-icon-on .topbar-dialog,
         $('#roomtitle + div').not('#roomdesc').appendTo('#roomdesc');
         $('#sidebar-menu').append(`<span> | <a id="room-transcript" title="view room transcript" href="/transcript/${roomId}">transcript</a> | <a id="room-owners" title="view room owners" href="/rooms/info/${roomId}/?tab=access#access-section-owner">owners</a></span>`);
         addLinksToOtherChatDomains();
-        reapplyPersistentChanges();
 
         // Occasionally reapply changes
         setInterval(reapplyPersistentChanges, 3000);
