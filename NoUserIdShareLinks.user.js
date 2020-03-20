62134328f1e16b10474ad44bc26e97e7de7973ed
Samuel Liew <samliew@gmail.com>
2020-03-20 16:40:56 +0800

    No need for event

--- NoUserIdShareLinks.user.js
+++ NoUserIdShareLinks.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         No User Id Share Links
-// @description  Removes your user ID from post share links
+// @name         No User Id in Share Links
+// @description  Adds option to remove your user ID from post share links
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      0.1
+// @version      0.2
 //
 // @include      https://*stackoverflow.com/*
 // @include      https://*serverfault.com/*
@@ -11,20 +11,37 @@
 // @include      https://*askubuntu.com/*
 // @include      https://*mathoverflow.net/*
 // @include      https://*.stackexchange.com/*
-// @include      https://stackapps.com/*
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    $(document.body).on('click', '.js-share-link', function() {
-        const popover = $(this).next();
-        setTimeout(function(pop) {
-            pop.find('input').val((i, v) => v.replace(/(\/\d+)\/\d+/, '$1'));
-            pop.find('.js-subtitle').remove();
-        }, 10, popover);
-    }).on('dblclick', '.s-popover[id^="se-share"] input', function() {
-        this.select();
-    });
+
+    function doPageload() {
+
+        // Strip user ids from the link itself
+        const sharelinks = $('.js-share-link').attr('href', (i, v) => v.replace(/(\/\d+)\/\d+/, '$1'));
+
+        // Strip user ids from the popups
+        sharelinks.next().find('input').val((i, v) => v.replace(/(\/\d+)\/\d+/, '$1'));
+    }
+
+
+    function appendStyles() {
+
+        const styles = `
+<style>
+.js-share-link + .s-popover .js-subtitle {
+    display: none;
+}
+</style>
+`;
+        $('body').append(styles);
+    }
+
+
+    // On page load
+    appendStyles();
+    doPageload();
 
 })();
