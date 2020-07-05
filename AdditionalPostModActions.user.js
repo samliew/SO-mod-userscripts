7c1170ae14aee8a55ce35d803d8ca15838e86f03
Samuel Liew <samliew@gmail.com>
2020-07-05 16:17:04 +0800

    Version bump

--- AdditionalPostModActions.user.js
+++ AdditionalPostModActions.user.js
@@ -3,7 +3,7 @@
 // @description  Adds a menu with mod-only quick actions in post sidebar
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      2.12
+// @version      2.12.1
 //
 // @include      https://*stackoverflow.com/*
 // @include      https://*serverfault.com/*
