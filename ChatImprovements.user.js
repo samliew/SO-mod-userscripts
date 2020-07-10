348214c5254ae34c8091ba877f6f2da10c07d79e
Samuel Liew <samliew@gmail.com>
2020-07-10 11:27:00 +0800

    Bugfix with missing recent pagination

--- ChatImprovements.user.js
+++ ChatImprovements.user.js
@@ -3,7 +3,7 @@
 // @description  New responsive userlist with usernames and total count, more timestamps, use small signatures only, mods with diamonds, message parser (smart links), timestamps on every message, collapse room description and room tags, mobile improvements, expand starred messages on hover, highlight occurances of same user link, room owner changelog, pretty print styles, and more...
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      2.18.2
+// @version      2.18.3
 //
 // @include      https://chat.stackoverflow.com/*
 // @include      https://chat.stackexchange.com/*
@@ -1470,7 +1470,7 @@ a.topbar-icon.topbar-icon-on .topbar-dialog,
             $('.content a[title]').attr('title', (i, v) => v.replace(/⌂/g, ' '));
         }
         // When viewing user pages
-        else if(/\/users\/\d+\//.test(location.pathname)) {
+        else if(/\/users\/\d+/.test(location.pathname)) {
 
             defaultRepliesLinkRange();
 
