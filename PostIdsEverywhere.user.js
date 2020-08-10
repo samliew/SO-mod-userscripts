a923b953f9be15b1fc9e0cf1fd217cb81399198f
Samuel Liew <samliew@gmail.com>
2020-08-10 09:04:23 +0800

    More specific selectors for dupe lists

--- PostIdsEverywhere.user.js
+++ PostIdsEverywhere.user.js
@@ -3,7 +3,7 @@
 // @description  Inserts post IDs everywhere where there's a post or post link
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.11.2
+// @version      1.11.3
 //
 // @include      https://*stackoverflow.com/*
 // @include      https://*serverfault.com/*
@@ -47,7 +47,7 @@
 
         // Lists
         const modQueuePostLinks = $('.js-body-loader').find('a:first');
-        $('a.question-hyperlink, a.answer-hyperlink, .js-post-title-link, .originals-of-duplicate a').add(modQueuePostLinks)
+        $('a.question-hyperlink, a.answer-hyperlink, .js-post-title-link, .originals-of-duplicate li > a, .originals-of-duplicate .js-originals-list > a').add(modQueuePostLinks)
             .not('.js-somu-post-ids').addClass('js-somu-post-ids')
             .each((i,el) => {
                 if(el.href.includes('/election')) return;
