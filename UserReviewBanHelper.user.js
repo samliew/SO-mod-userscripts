b21b51f5bfc7cfa876729799f8cd3c6c7aa13407
Samuel Liew <samliew@gmail.com>
2020-10-27 16:42:51 +0800

    Update for removal of css from Stacks

--- UserReviewBanHelper.user.js
+++ UserReviewBanHelper.user.js
@@ -3,7 +3,7 @@
 // @description  Display users' prior review bans in review, Insert review ban button in user review ban history page, Load ban form for user if user ID passed via hash
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      5.5.2
+// @version      5.6
 //
 // @include      */review/close*
 // @include      */review/reopen*
@@ -269,27 +269,24 @@
             }).appendTo('.js-review-instructions');
 
             // Add review-ban button for users who selected "Looks OK"
-            $(`<button class="mt16">Review ban "Looks OK"</button>`).appendTo('.reviewable-post-stats')
-                .click(function() {
+            $(`<button class="s-btn s-btn__sm s-btn__filled mt12">Review ban "Looks OK"</button>`).appendTo('.reviewable-post-stats').click(function() {
                 $('.review-results').filter((i, el) => el.innerText.includes('Looks OK')).find('.reviewban-link').each((i, el) => el.click());
                 $(this).remove();
-            });
+            }).wrap('<div></div>');
 
             // Add review-ban button for users who selected "Requires Editing"
-            $(`<button class="mt16">Review ban "Requires Editing"</button>`).appendTo('.reviewable-post-stats')
-                .click(function() {
+            $(`<button class="s-btn s-btn__sm s-btn__filled mt12">Review ban "Requires Editing"</button>`).appendTo('.reviewable-post-stats').click(function() {
                 $('.review-results').filter((i, el) => el.innerText.includes('Requires Editing')).find('.reviewban-link').each((i, el) => el.click());
                 $(this).remove();
                 unsafeWindow.top.close();
-            });
+            }).wrap('<div></div>');
 
             // Add review ban all button
-            $(`<button class="mt16">Review ban ALL</button>`).appendTo('.reviewable-post-stats')
-                .click(function() {
+            $(`<button class="s-btn s-btn__sm s-btn__filled mt12">Review ban ALL</button>`).appendTo('.reviewable-post-stats').click(function() {
                 $(this).siblings('button').click();
                 $(this).remove();
                 unsafeWindow.top.close();
-            });
+            }).wrap('<div></div>');
         }
 
         // Get users review history
