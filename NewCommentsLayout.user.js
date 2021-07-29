bf03cc0bd2f95965c5d4cf7b3f28443541fe9e65
Samuel Liew <samliew@gmail.com>
2021-07-29 16:12:28 +0800

    Fix new mod/staff badges

--- NewCommentsLayout.user.js
+++ NewCommentsLayout.user.js
@@ -3,7 +3,7 @@
 // @description  Better comments layout for easier readability and moderation
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      1.1.1
+// @version      1.2
 //
 // @include      https://*stackoverflow.com/*
 // @include      https://*serverfault.com/*
@@ -61,6 +61,7 @@ ul.comments-list .comment-score span,
 }
 .comment-copy + .comment-user {
     margin-left: 5px;
+    margin-right: 5px;
 }
 .comment-user {
     font-style: italic;
@@ -87,6 +88,36 @@ ul.comments-list .comment .comment-text {
 ul.comments-list .comment:not(.deleted-comment):hover .comment-actions,
 ul.comments-list .comment:not(.deleted-comment):hover .comment-text {
     background-color: var(--yellow-050);
+}
+
+/* New mod/staff badges */
+.s-badge__staff {
+  border-color: var(--orange-400) !important;
+  background-color: transparent !important;
+  color: var(--orange-400) !important;
+}
+.s-badge__moderator {
+  border-color: var(--theme-secondary-color) !important;
+  background-color: transparent !important;
+  color: var(--theme-secondary-color) !important;
+}
+.s-badge__moderator.s-badge__xs:before {
+  background-image: url("data:image/svg+xml,%3Csvg width='7' height='9' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M2.798.246c.3-.329 1.1-.327 1.399 0l2.579 3.66c.3.329.298.864 0 1.192L4.196 8.75c-.299.329-1.1.327-1.398 0L.224 5.098a.904.904 0 010-1.192L2.798.246z' fill='%230077cc'/%3E%3C/svg%3E") !important;
+}
+.s-badge__moderator:before {
+  background-image: url("data:image/svg+xml,%3Csvg width='12' height='14' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M5.528.746c.257-.329.675-.327.93 0l4.42 5.66c.258.329.257.864 0 1.192l-4.42 5.66c-.256.328-.674.327-.93 0l-4.42-5.66c-.257-.329-.256-.865 0-1.192l4.42-5.66z' fill='%230077cc'/%3E%3C/svg%3E") !important;
+}
+.comment-body > .s-badge {
+    position: relative;
+    display: inline-block;
+    white-space: nowrap;
+    margin: 0px 0px 5px;
+}
+.comment-body > .s-badge + .comment-date {
+    margin-left: 6px;
+}
+.s-badge__moderator:before {
+    display: inline-block !important;
 }
 
 `);
