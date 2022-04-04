dcbdf785bc9c20f780391bd38fe7fa74532069cb
Samuel Liew <samliew@gmail.com>
2022-04-04 09:35:25 +0800

    Make tag burnination an SO-only template

--- ModMessageHelper.user.js
+++ ModMessageHelper.user.js
@@ -3,7 +3,7 @@
 // @description  Adds menu to quickly send mod messages to users
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      2.7
+// @version      2.8
 //
 // @include      https://*stackoverflow.com/*
 // @include      https://*serverfault.com/*
@@ -74,16 +74,6 @@ First, it reduces the amount of noise on the site, since the message is displaye
 In the best case, comments like these are merely noise, redundant with system-level notifications; in the worst case, they may be perceived as an attempt to pressure someone to do something that is, after all, completely optional.`,
     },
     {
-        templateName: "ban evasion, multiple accounts",
-        suspensionReason: "for rule violations",
-        suspensionDefaultDays: 30,
-        templateBody: `All system and moderator-imposed limits/blocks/bans/suspensions/etc. apply to the user, not just a single account. You are not permitted to create one or more new accounts in order to get around such limitations. If you are hitting a limit on one account, then you should act as if you were hitting that limit on each of your accounts. The additional accounts which have been used to evade such limitations will be removed.
-
-The most common limitations for people to attempt to evade are the system imposed question and answer bans. When you're getting the message 'We are no longer accepting questions/answers from this account', then you should act as if you are getting that message on all of your accounts and not post additional questions or answers (whichever you're hitting), even if you have an alternate account which is not banned. Question and answer bans are not the only restrictions which apply to all of a user's accounts, but it's the most common one which you might be, or might have been, attempting to get around. For more detail about question and answer bans and what you can do to get out of them, please see [What can I do when getting “We are no longer accepting questions/answers from this account”?](https://meta.stackoverflow.com/a/255584#255584)
-
-Having more than one account is permitted, if the additional account is not used to circumvent such limitations and the accounts do not interact with each other, or otherwise allow you to do things which you would not be permitted to do with a single account. If you are interested in more information about having more than one account, please see [What are the rules governing multiple accounts (i.e. sockpuppets)?](https://meta.stackoverflow.com/q/388984)`,
-    },
-    {
         templateName: "minor edits bumping post",
         suspensionReason: "for rule violations",
         suspensionDefaultDays: 0,
@@ -114,6 +104,7 @@ Please note that we still require full attribution with a link to the external s
 Thank you, and we look forward to your contributions in the future.`,
     },
     {
+        soOnly: true, // because template has SO-only meta links
         templateName: "self tag burnination",
         suspensionReason: "for rule violations",
         suspensionDefaultDays: 0,
@@ -122,6 +113,16 @@ Thank you, and we look forward to your contributions in the future.`,
 You have recently removed many tags from questions without following the burnination process. Do not do that. This message is a warning. If you do this again, with this or any other tag, then there will be further consequences.
 
 The edits you made will be reverted. Some of the edits have other beneficial changes, which you are welcome to reapply. However, you are not permitted to systematically remove tags from questions without following the burnination process.`,
+    },
+    {
+        templateName: "ban evasion, multiple accounts",
+        suspensionReason: "for rule violations",
+        suspensionDefaultDays: 30,
+        templateBody: `All system and moderator-imposed limits/blocks/bans/suspensions/etc. apply to the user, not just a single account. You are not permitted to create one or more new accounts in order to get around such limitations. If you are hitting a limit on one account, then you should act as if you were hitting that limit on each of your accounts. The additional accounts which have been used to evade such limitations will be removed.
+
+The most common limitations for people to attempt to evade are the system imposed question and answer bans. When you're getting the message 'We are no longer accepting questions/answers from this account', then you should act as if you are getting that message on all of your accounts and not post additional questions or answers (whichever you're hitting), even if you have an alternate account which is not banned. Question and answer bans are not the only restrictions which apply to all of a user's accounts, but it's the most common one which you might be, or might have been, attempting to get around. For more detail about question and answer bans and what you can do to get out of them, please see [What can I do when getting “We are no longer accepting questions/answers from this account”?](https://meta.stackoverflow.com/a/255584#255584)
+
+Having more than one account is permitted, if the additional account is not used to circumvent such limitations and the accounts do not interact with each other, or otherwise allow you to do things which you would not be permitted to do with a single account. If you are interested in more information about having more than one account, please see [What are the rules governing multiple accounts (i.e. sockpuppets)?](https://meta.stackoverflow.com/q/388984)`,
     },
     {
         templateName: "account sharing",
@@ -156,6 +157,7 @@ We wish you a pleasant vacation from the site, and we look forward to your retur
         //addPrefix: false,
         //addSuffix: false,
         //addSignature: false,
+        // soOnly: false, // if template has SO-only meta links
     },
     */
 ];
@@ -692,12 +694,12 @@ function appendModMessageMenu() {
         menuitems += `<a target="_blank" href="${modMessageLink}?action=sockpuppet-upvoting">sockpuppet upvoting</a>`;
         menuitems += `<a target="_blank" href="${modMessageLink}?action=targeted-votes">targeted votes</a>`;
         menuitems += `<a target="_blank" href="${modMessageLink}?action=revenge-downvoting">revenge downvoting</a>`;
-        menuitems += `<a target="_blank" href="${modMessageLink}?action=ban-evasion">ban evasion</a>`;
 
         // Add custom reasons
         if (customModMessages.length > 0) {
             menuitems += `<div class="separator"></div>`;
             customModMessages.forEach(v => {
+                if(v.soOnly === true && !isSO) return; // Don't add menu item if SO-only template and not SO
                 menuitems += `<a target="_blank" href="${modMessageLink}?action=${v.templateName.replace(/\W/g, '').toLowerCase()}">${v.templateName}</a>`;
             });
         }
