82d1f857483ce8086102095f00b422590294344a
Samuel Liew <samuel.liew@staff.pentanet.com.au>
2022-04-08 10:46:24 +0800

    Update user annotations table, add read count

--- UserHistoryImprovements.user.js
+++ UserHistoryImprovements.user.js
@@ -3,7 +3,7 @@
 // @description  Fixes broken links in user annotations, and minor layout improvements
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      2.2
+// @version      2.3
 //
 // @include      https://*stackoverflow.com/users/history/*
 // @include      https://*serverfault.com/users/history/*
@@ -13,12 +13,9 @@
 // @include      https://*.stackexchange.com/users/history/*
 // ==/UserScript==
 
-/* globals StackExchange, GM_info */
+/* globals StackExchange */
 
 'use strict';
-
-// Moderator check
-if (typeof StackExchange == "undefined" || !StackExchange.options || !StackExchange.options.user || !StackExchange.options.user.isModerator) return;
 
 if (unsafeWindow !== undefined && window !== unsafeWindow) {
     window.jQuery = unsafeWindow.jQuery;
@@ -74,22 +71,53 @@ function doPageLoad() {
     });
 
     // Prettify annotations table
-    $('#annotations tr').each(function () {
-        const td = $(this).children('td, th').eq(3);
+    const table = $('#annotations table').addClass('s-table');
 
-        // Pad dates to same length
-        const date = $(this).children('td, th').eq(2).find('span');
-        date.text((i, v) => v.replace(/\s(\d)\b/g, ' 0$1'));
+    // Read indicator
+    table.find('thead th').last().text('Read');
+
+    // Message
+    $('tbody tr', table).each(function () {
+
+        // Shorten type
+        const type = $('tr', table).children('td:first-child').text((i, v) => v.trim().slice(0, 4)).text();
+
+        // Shorten timestamps, pad single digits
+        $(this).find('.relativetime').text((i, v) => v.replace(/ at .+$/, '').replace(/ (\d),/, ' 0$1,'));
+
+        // Get message
+        const td = $(this).children('td').eq(3);
 
         // Special class for message/suspension rows
-        const aType = $(this).children().first().text().trim().toLowerCase();
-        const aHasLink = $(this).children().eq(3).find('a').length > 0;
-        if (/(suspension|message)/.test(aType) && aHasLink) {
+        const msgLink = $(this).children().eq(3).find('a');
+        const aHasLink = $(this).children().eq(3).find('a').length === 1;
+        if (/^\s?(susp|mess)/i.test(type) && aHasLink) {
             $(this).addClass('user-message');
+
+            // Read status cell
+            const status = $('<span class="num-msgs"></span>').appendTo($(this).find('td').last().empty());
+
+            // Get user message and update status
+            const url = msgLink.attr('href');
+            $.get(url).done(function(page) {
+                const messages = $('#mainbar .msg', page);
+                const modMessages = messages.filter('.msg-moderator', page);
+                const readModMessages = $('.msg-audit', modMessages).filter((i, el) => el.children.length == 2);
+
+                const numModMessages = modMessages.length;
+                const numReadModMessages = readModMessages.length;
+
+                console.log(messages, numModMessages, numReadModMessages);
+
+                const color = numModMessages !== numReadModMessages ? 'fc-red-600' : '';
+                status.text(`${numReadModMessages}/${numModMessages}`);
+                status.addClass(color);
+            });
+
             return;
         }
 
-        // Fix broken links
+        // Fix broken links in annotations
         const str = td.html()
             .replace(/" title="([^"]+)/g, '') // remove title attribute
             .replace(/" rel="nofollow noreferrer/g, '') // remove auto-inserted rel attribute from external links
@@ -173,13 +201,16 @@ styles.innerHTML = `
 }
 #annotations th {
     font-weight: bold;
-    border-bottom: 1px solid var(--black-300);
+    font-size: 1em;
 }
 #annotations th,
 #annotations td {
-    padding: 7px 5px;
     text-overflow: ellipsis;
     overflow: hidden;
+    vertical-align: top;
+}
+#annotations td {
+    font-size: 0.9em;
 }
 #annotations td:nth-child(-n + 3) {
     white-space: nowrap;
@@ -187,32 +218,36 @@ styles.innerHTML = `
 #annotations td:nth-child(1) {
     max-width: 52px;
 }
+#annotations td:nth-child(2) {
+    max-width: 90px;
+}
 #annotations td:nth-child(3) {
     max-width: 95px;
+    text-align: right;
 }
 #annotations td:nth-child(4) {
     width: 100%;
 }
 #annotations td:nth-child(5) {
-    min-width: 150px;
-    max-width: 150px;
+    min-width: 200px;
+    max-width: 200px;
 }
-#annotations th:nth-child(6),
 #annotations td:nth-child(6) {
-    display: none;
+    text-align: center;
 }
-#annotations tbody tr:nth-child(odd) {
-    background-color: var(--black-050);
+.s-table tbody tr:nth-child(even),
+#annotations tbody tr:nth-child(even) {
+    background-color: var(--black-025);
 }
 #annotations table.annotation {
     border: 1px solid var(--black-100);
 }
 #annotations tr.user-message td:nth-child(4) {
     padding-left: 25px;
-    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path></svg>') left 5px top 7px/14px no-repeat;
+    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path></svg>') left 5px top 3px/14px no-repeat;
 }
 body.SOMU-SEDM #annotations tr.user-message td:nth-child(4) {
-    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path stroke="white" fill="white" d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path></svg>') left 5px top 7px/14px no-repeat;
+    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path stroke="white" fill="white" d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path></svg>') left 5px top 3px/14px no-repeat;
 }
 
 #change-filter {
