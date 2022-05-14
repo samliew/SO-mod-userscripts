5d98c24c8bb10d79e6db01a1613b8f0a09f966a9
Oleg Valter <o.a.philippov@gmail.com>
2022-05-14 04:01:40 +0300

    bumped RQH to 4.7

--- ReviewQueueHelper.user.js
+++ ReviewQueueHelper.user.js
@@ -3,7 +3,7 @@
 // @description  Keyboard shortcuts, skips accepted questions and audits (to save review quota)
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      4.6
+// @version      4.7
 //
 // @include      https://*stackoverflow.com/review/*
 // @include      https://*serverfault.com/review/*
