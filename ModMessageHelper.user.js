faa99139bbe5a039678883c3f33688362a42543e
Makyen <makyen@users.noreply.github.com>
2022-10-21 12:57:44 -0700

    MMH: appendModMessageMenu refactor userbox

--- ModMessageHelper.user.js
+++ ModMessageHelper.user.js
@@ -830,10 +830,11 @@ function appendModMessageMenu() {
         .not('.js-mod-message-menu')
         .addClass('js-mod-message-menu')
         .each(function () {
+            const userbox = $(this);
 
             let uid = 0;
             try {
-                uid = ($(this).find('a[href^="/users/"]').attr('href') || '/0/').match(/\/(\d+)\//)[1];
+                uid = (userbox.find('a[href^="/users/"]').attr('href') || '/0/').match(/\/(\d+)\//)[1];
                 uid = Number(uid);
                 this.dataset.uid = uid;
             }
@@ -843,14 +844,13 @@ function appendModMessageMenu() {
             // if user is self, ignore
             if (uid == StackExchange.options.user.userId) return;
 
-            const post = $(this).closest('.question, .answer');
+            const post = userbox.closest('.question, .answer');
             const pid = post.attr('data-questionid') || post.attr('data-answerid');
 
-            const userbox = $(this);
             const userlink = userbox.find('a').attr('href');
             const userrep = userbox.find('.reputation-score').text();
             const username = userbox.find('.user-details a').first().text();
-            const modFlair = $(this).find('.mod-flair');
+            const modFlair = userbox.find('.mod-flair');
 
             if (uid == -1 || modFlair.length == 1) return;
 
@@ -908,7 +908,7 @@ function appendModMessageMenu() {
             cmMenuitems += `<a target="_blank" href="${cmMessageLink}?action=other">other...</a>`;
 
 
-            $(this).append(`
+            userbox.append(`
 <div class="js-mod-message-link flex--item s-btn ta-center py8 somu-mod-message-link ${modMenuOnClick ? 'click-only' : ''}" data-shortcut="O" title="Contact...">
   <svg aria-hidden="true" role="img" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-icon mln1 mr0">
     <path fill="currentColor" d="M464 64H48C21.5 64 0 85.5 0 112v288c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zM48 96h416c8.8 0 16 7.2 16 16v41.4c-21.9 18.5-53.2 44-150.6 121.3-16.9 13.4-50.2 45.7-73.4 45.3-23.2.4-56.6-31.9-73.4-45.3C85.2 197.4 53.9 171.9 32 153.4V112c0-8.8 7.2-16 16-16zm416 320H48c-8.8 0-16-7.2-16-16V195c22.8 18.7 58.8 47.6 130.7 104.7 20.5 16.4 56.7 52.5 93.3 52.3 36.4.3 72.3-35.5 93.3-52.3 71.9-57.1 107.9-86 130.7-104.7v205c0 8.8-7.2 16-16 16z" class="" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"></path>
