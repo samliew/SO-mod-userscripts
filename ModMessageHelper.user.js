015cd365f624931db29a08ba2fe2cb4a8588cb70
Makyen <makyen@users.noreply.github.com>
2022-10-26 03:01:56 -0700

    ModMessageHelper: version 3.3.2

--- ModMessageHelper.user.js
+++ ModMessageHelper.user.js
@@ -3,7 +3,7 @@
 // @description  Adds menu to quickly send mod messages to users
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      3.3.1
+// @version      3.3.2
 //
 // @match        *://*.askubuntu.com/*
 // @match        *://*.mathoverflow.net/*
