0c552ed4fd3d2c2749798a4c1ca55b739d119d42
Oleg Valter <o.a.philippov@gmail.com>
2022-12-20 22:34:53 +0300

    bumped CI to version 3.6.2

--- ChatImprovements.user.js
+++ ChatImprovements.user.js
@@ -3,7 +3,7 @@
 // @description  New responsive userlist with usernames and total count, more timestamps, use small signatures only, mods with diamonds, message parser (smart links), timestamps on every message, collapse room description and room tags, mobile improvements, expand starred messages on hover, highlight occurances of same user link, room owner changelog, pretty print styles, and more...
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       @samliew
-// @version      3.6.1
+// @version      3.6.2
 //
 // @include      https://chat.stackoverflow.com/*
 // @include      https://chat.stackexchange.com/*
