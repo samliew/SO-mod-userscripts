e1777b880ec8a993aa93a76a88e931278c215529
Samuel Liew <samliew@gmail.com>
2023-04-03 23:59:18 +0800

    Update common.js, globals.d.ts

--- globals.d.ts
+++ globals.d.ts
@@ -13,9 +13,6 @@ interface SOMU {
     addOption(scriptName: string, optionName: string, defaultValue?: string, dataType?: "string"): boolean | undefined;
     addOption(scriptName: string, optionName: string, defaultValue?: string, dataType?: OptionType): boolean | undefined;
 
-    appendStyles(): void;
-    handleSidebarEvents(): void;
-
     getOptionValue(scriptName: string, optionName: string, defaultValue?: boolean, dataType?: "bool"): boolean;
     getOptionValue(scriptName: string, optionName: string, defaultValue?: number, dataType?: "int"): number;
     getOptionValue(scriptName: string, optionName: string, defaultValue?: string, dataType?: "string"): string;
@@ -23,18 +20,14 @@ interface SOMU {
 
     saveOptionValue(key: string, value: string): void;
 
+    handleSidebarEvents(): void;
+    appendStyles(): void;
+
     init(): void;
 }
 
 declare var SOMU: SOMU;
 declare var Store: typeof import("@userscripters/storage");
-
-declare function ajaxPromise(opts: string | object, type?: string): Promise<Document>;
-declare function isModerator(): boolean;
-declare function hasInvalidIds(): boolean;
-declare function htmlDecode(input: string): string;
-declare function jQueryXhrOverride(): XMLHttpRequest;
-declare function addBackoff(sec: number): void;
 
 interface Window {
     jQuery: JQueryStatic;
@@ -42,3 +35,95 @@ interface Window {
     SOMU: SOMU | undefined;
     Store: typeof Store | undefined;
 }
+
+/**
+ * ================================
+ * Constant Variables
+ * ================================
+ */
+declare var scriptName: string;
+declare var scriptSlug: string;
+declare var _window: Window;
+declare var store: WindowLocalStorage;
+
+declare var monthsOfYear: string[];
+declare var daysOfWeek: string[];
+declare var MS: object;
+
+declare var _hostname: string;
+declare var isMSE: boolean;
+declare var isSO: boolean;
+declare var isSOMeta: boolean;
+declare var isMetaSite: boolean;
+declare var parentUrl: string;
+declare var metaUrl: string | undefined;
+declare var siteApiSlug: string;
+
+declare var userId: number | null;
+declare var fkey: string | null;
+
+/**
+ * ================================
+ * Date Functions
+ * ================================
+ */
+declare function seApiDateToDate(apiDate: string): Date | null;
+declare function dateToIsoString(date: Date): string;
+declare function dateToRelativeString(date: Date): string;
+
+/**
+ * ================================
+ * Validation/Boolean Functions
+ * ================================
+ */
+declare function isModerator(): boolean;
+
+/**
+ * ================================
+ * AJAX / Callback / Promise Functions
+ * ================================
+ */
+declare function delay(ms: number): Promise<void>;
+declare function ajaxPromise(opts: string | object, type?: string): Promise<Document>;
+declare function jQueryXhrOverride(): XMLHttpRequest;
+declare function hasBackoff(): boolean;
+declare function addBackoff(sec: number): void;
+
+/**
+ * ================================
+ * String / HTML Parsing / Validation Functions
+ * ================================
+ */
+declare function htmlDecode(input: string): string;
+
+/**
+ * ================================
+ * Post ID Functions
+ * ================================
+ */
+declare function hasInvalidIds(): boolean;
+declare function getPostId(url: string): number | null;
+
+/**
+ * ================================
+ * Location and History Functions
+ * ================================
+ */
+declare function getQueryParam(key: string): string;
+declare function goToPost(pid: number): void;
+
+/**
+ * ================================
+ * DOM Manipulation Functions
+ * ================================
+ */
+declare function setAttributes(el: HTMLElement, attrs: object): void;
+declare function makeElem(tagName: string, attrs?: object, text?: string, children?: HTMLElement[]): HTMLElement;
+declare function addStylesheet(css: string, atDocumentEnd?: boolean): void;
+
+/**
+ * ================================
+ * Observer / Event Functions
+ * ================================
+ */
+declare function waitForElement(selector: string, context: HTMLElement | Document): Promise<void>
