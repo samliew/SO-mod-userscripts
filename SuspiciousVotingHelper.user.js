3ab9b51db5629021542561f2b1de5a68d863a53b
Samuel Liew <samliew@gmail.com>
2023-05-29 14:31:28 +0800

    Improve activity table page

--- SuspiciousVotingHelper.user.js
+++ SuspiciousVotingHelper.user.js
@@ -3,7 +3,7 @@
 // @description  Assists in building suspicious votes CM messages. Highlight same users across IPxref table. Also provides support for SEDE query https://data.stackexchange.com/stackoverflow/query/968803
 // @homepage     https://github.com/samliew/SO-mod-userscripts
 // @author       Samuel Liew
-// @version      3.4.2
+// @version      3.5
 //
 // @match        https://*.stackoverflow.com/*
 // @match        https://*.serverfault.com/*
@@ -36,6 +36,7 @@ if (!isModerator()) return;
 
 const apikey = 'yZcUvuGAMj25rYZ)a5YNqg((';
 const newlines = '\n\n';
+const activityGraphCols = 5;
 
 
 // jQuery mapping functions to build object from table rows in votes tables page for suspicious votes canned message
@@ -77,6 +78,88 @@ function mapInvVotePatternItemsToObject() {
     size: vNum >= 5 ? 'large' : '',
     used: false,
   }
+}
+
+
+function splitUserActivity() {
+  const comp = $('.compare').first();
+
+  // Insert buttons to first comp
+  comp.find('.user-td').not('.js-arrow-init').addClass('js-arrow-init').append(`
+<div class="arrow-container">
+  <button class="s-btn s-btn__xs js-up-arrow" title="Move up">▲</button>
+  <button class="s-btn s-btn__xs js-remove-row" title="Remove user">▬</button>
+  <button class="s-btn s-btn__xs js-down-arrow" title="Move down">▼</button>
+</div>
+`);
+
+  // Only run split once
+  if (comp.hasClass('js-split-init')) return;
+  comp.addClass('js-split-init');
+
+  const table = comp.find('table.activity-graph');
+  const cols = table.find('thead td').length;
+
+  // Duplicate compare for every X columns
+  const num = Math.ceil(cols / activityGraphCols);
+  for (let i = 1; i < num; i++) {
+    comp.clone().insertAfter(comp);
+  }
+
+  // Event listeners for new up-arrow, down-arrow and remove-row
+  $('#mainbar')
+    .on('click', '.js-up-arrow', function () {
+      const row = $(this).closest('tr');
+      const index = row.index();
+      const uid = row[0].dataset.userId;
+
+      // If first row, do nothing
+      if (index === 0) return false;
+
+      // Get all rows with same uid
+      const rows = $(`tr[data-user-id="${uid}"]`);
+
+      // Move all rows up one
+      rows.each(function () {
+        const row = $(this);
+        row.insertBefore(row.prev());
+      });
+
+      return false;
+    }).on('click', '.js-down-arrow', function () {
+      const row = $(this).closest('tr');
+      const index = row.index();
+      const uid = row[0].dataset.userId;
+
+      // If last row, do nothing
+      if (index === row.parent().children().length - 1) return false;
+
+      // Get all rows with same uid
+      const rows = $(`tr[data-user-id="${uid}"]`);
+
+      // Move all rows down one
+      rows.each(function () {
+        const row = $(this);
+        row.insertAfter(row.next());
+      });
+
+      return false;
+    }).on('click', '.js-remove-row', function () {
+      const row = $(this).closest('tr');
+      const siblings = row.siblings();
+      const uid = row[0].dataset.userId;
+
+      // If only one row, do nothing
+      if (siblings.length === 0) return false;
+
+      // Get all rows with same uid
+      const rows = $(`tr[data-user-id="${uid}"]`);
+
+      // Remove all rows
+      rows.remove();
+
+      return false;
+    });
 }
 
 
@@ -347,6 +430,47 @@ function doPageLoad() {
     singleUserRows.reverse().appendTo('#xref-ids > tbody');
   }
 
+  // If on user-activity page
+  else if (location.pathname.includes('/admin/user-activity')) {
+
+    // Add some styles to expand max-width
+    addStylesheet(`
+html.html__unpinned-leftnav body>.container,
+#content {
+  max-width: 1440px;
+}
+#mainbar {
+  width: auto !important;
+  float: none;
+}
+`);
+
+    // Unpin left sidebar
+    $('html').addClass('html__unpinned-leftnav');
+    const pinnedSidebar = $('.js-pinned-left-sidebar').removeAttr('data-can-be').attr({
+      'data-is-here-when': '',
+      'id': ''
+    });
+    const unpinnedSidebar = $('.js-unpinned-left-sidebar').removeAttr('data-can-be').attr({
+      'data-is-here-when': 'sm md lg',
+      'id': 'left-sidebar'
+    });
+    pinnedSidebar.children().appendTo(unpinnedSidebar);
+
+    // Move sidebar contents to mainbar
+    const ipLegendAside = $(`<aside></aside>`).insertBefore('.compare');
+    $('#sidebar .module.ip-legend').appendTo(ipLegendAside);
+    $('#sidebar').remove();
+    $('#mainbar').addClass('w100').prepend(`
+<aside class="mb8">
+  <span>Add users to compare their activity times.</span>
+  <span>This tool is meant for correlating activity between suspected sock puppet accounts.</span>
+</aside>`);
+
+    // On ajaxStop
+    $(document).ajaxStop(splitUserActivity);
+  }
+
   // If on user votes page
   else if (location.pathname.includes('/show-user-votes/')) {
     const svhSortClass = 'bg-yellow-100';
@@ -566,6 +690,158 @@ tr[data-uid].svh-curruser {
 /* Invalidated vote tables */
 #mainbar-full > .flex__allitems6:last-child > .flex--item > table {
 
+}
+
+/* user-activity page improvements */
+.user-activity {
+
+}
+.user-activity .user-cards thead td {
+  height: 22px;
+}
+.user-activity .user-td {
+  position: relative;
+  height: 75px;
+  padding-top: 1px;
+  background-color: transparent;
+}
+.user-activity .user-td.row-odd {
+  background-color: var(--black-050);
+}
+.user-activity .user-td.row-even {
+  background-color: var(--white);
+}
+.user-activity .compare {
+  width: auto;
+  margin-left: 1.5rem;
+  margin-bottom: 2rem;
+}
+.user-activity .day-header {
+  height: 22px;
+  font-size: 1.2rem;
+  font-weight: 600;
+  text-align: center;
+  vertical-align: middle;
+  background-color: var(--black-150);
+}
+.user-activity .row-odd {
+  background-color: var(--black-050);
+}
+.user-activity .row-even {
+  background-color: var(--white);
+}
+.user-activity .compare table,
+.user-activity .compare table td,
+.user-activity .compare table th {
+  position: relative;
+  border-color: var(--black-150) !important;
+}
+.user-activity .compare .activity-graph {
+  border-left: 1px solid;
+  border-right: 1px solid;
+}
+.user-activity .compare .activity-graph th,
+.user-activity .compare .activity-graph td {
+  display: none;
+}
+.user-activity .compare:nth-of-type(1) .activity-graph th:is(:nth-child(1), :nth-child(2), :nth-child(3), :nth-child(4), :nth-child(5)),
+.user-activity .compare:nth-of-type(1) .activity-graph td:is(:nth-child(1), :nth-child(2), :nth-child(3), :nth-child(4), :nth-child(5)),
+.user-activity .compare:nth-of-type(2) .activity-graph th:is(:nth-child(6), :nth-child(7), :nth-child(8), :nth-child(9), :nth-child(10)),
+.user-activity .compare:nth-of-type(2) .activity-graph td:is(:nth-child(6), :nth-child(7), :nth-child(8), :nth-child(9), :nth-child(10)),
+.user-activity .compare:nth-of-type(3) .activity-graph th:is(:nth-child(11), :nth-child(12), :nth-child(13), :nth-child(14), :nth-child(15)),
+.user-activity .compare:nth-of-type(3) .activity-graph td:is(:nth-child(11), :nth-child(12), :nth-child(13), :nth-child(14), :nth-child(15)) {
+  display: table-cell;
+}
+.user-activity .scroller {
+  width: auto;
+}
+.user-activity .day-even,
+.user-activity .day-odd {
+  height: 75px;
+}
+.user-activity .mspark {
+  transform: scaleY(1.5);
+  transform-origin: top;
+}
+.user-activity .mspark::before {
+  /* svg background with vertical lines for each hour */
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"><defs><pattern id="pattern" x="0" y="0" width="1" height="1"><line x1="0" y1="0" x2="0" y2="100%" stroke="var(--black-100)" stroke-width="1" /></pattern></defs><rect x="0" y="0" width="100%" height="100%" fill="url(pattern)" /></svg>');
+}
+.user-activity .msbar {
+  min-height: 3px;
+}
+.user-activity .user-container {
+  display: flex;
+  justify-content: space-evenly;
+  width: auto;
+}
+.user-activity .user-container .up-arrow,
+.user-activity .user-container .down-arrow,
+.user-activity .user-container .remove-row,
+.user-activity .compare:nth-of-type(n + 2) .arrow-container {
+  display: none;
+}
+.user-activity .user-td .arrow-container {
+  position: absolute;
+  right: 100%;
+  top: 50%;
+  transform: translate(-5px, -50%);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.user-activity .user-td .arrow-container .s-btn {
+  --_bu-p: 0.2em;
+  --_bu-fc: var(--black-700);
+  --_bu-fs: 0.7rem;
+}
+.user-activity .grav-container {
+  margin-bottom: -0.5rem;
+}
+.user-activity .user-container .grav-container::after {
+  content: attr(title);
+  overflow: hidden;
+  display: inline-block;
+  max-width: 32px;
+  white-space: nowrap;
+  font-size: 0.75rem;
+  line-height: 0.8;
+}
+.user-activity .ip-legend h4 {
+  display: none;
+}
+.user-activity .ip-legend .legend {
+  display: flex;
+  flex-wrap: wrap;
+}
+.user-activity .ip-legend .legend .ip-hover {
+  margin: 0 2rem 1rem 0;
+}
+.user-activity .ip-legend .legend .ip-hover span:nth-of-type(1) {
+  margin-right: 0.5rem;
+}
+.user-activity .ip-legend .legend .ip-hover span:nth-of-type(2) {
+  margin-left: 1.4rem;
+}
+.user-activity .ip-legend .legend .ip-hover ~ br {
+  display: none;
+}
+.user-activity hr {
+  margin-top: var(--su24);
+  margin-bottom: var(--su24);
+  background-color: var(--black-100);
+}
+.user-activity .permalink {
+  float: none !important;
+  margin-left: 1.2rem;
 }
 `); // end stylesheet
 
